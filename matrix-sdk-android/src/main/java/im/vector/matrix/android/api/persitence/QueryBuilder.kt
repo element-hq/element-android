0f4a62b7bfccc47765023954a543abc3f95e7afc
ganfra <francois.ganard@gmail.com>
2018-10-17 15:35:26 +0200

    Remove persistence package for the moment

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/api/persitence/QueryBuilder.kt
+++
@@ -1,7 +0,0 @@
-package im.vector.matrix.android.api.persitence
-
-interface QueryBuilder<DATA, QUERY : Query<DATA>> {
-
-    fun build(): QUERY
-
-}
--- matrix-sdk-android/src/main/java/im/vector/matrix/android/api/persitence/QueryDataObserver.java
+++
@@ -1,5 +0,0 @@
-package im.vector.matrix.android.api.persitence;
-
-public interface QueryDataObserver<DATA> {
-    void onData(DATA data);
-}
