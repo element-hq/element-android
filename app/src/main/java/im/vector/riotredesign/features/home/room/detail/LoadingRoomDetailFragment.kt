1cc19a90bc362ddf54dfc969356c5ce6759b70c9
ganfra <francois.ganard@gmail.com>
2018-11-02 10:30:59 +0100

    Add splash for initial sync

--- app/src/main/java/im/vector/riotredesign/features/home/room/detail/LoadingRoomDetailFragment.kt
+++ app/src/main/java/im/vector/riotredesign/features/home/room/detail/LoadingRoomDetailFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import im.vector.riotredesign.R
 import im.vector.riotredesign.core.platform.RiotFragment
+import kotlinx.android.synthetic.main.fragment_loading_room_detail.*
 
 class LoadingRoomDetailFragment : RiotFragment() {
 
@@ -18,6 +20,13 @@ class LoadingRoomDetailFragment : RiotFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_loading_room_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this)
+                .load(R.drawable.riot_splash)
+                .into(animatedLogoImageView)
     }
 
 
