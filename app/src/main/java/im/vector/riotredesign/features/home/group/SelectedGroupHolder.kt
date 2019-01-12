acf2301fc91920f0cb98a54a93895b18f7447c26
ganfra <francois.ganard@gmail.com>
2019-01-12 10:20:46 +0100

    Use RxRelay instead of Subjects

--- app/src/main/java/im/vector/riotredesign/features/home/group/SelectedGroupHolder.kt
+++ app/src/main/java/im/vector/riotredesign/features/home/group/SelectedGroupHolder.kt
@@ -1,17 +1,17 @@
 package im.vector.riotredesign.features.home.group
 
 import arrow.core.Option
+import com.jakewharton.rxrelay2.BehaviorRelay
 import im.vector.matrix.android.api.session.group.model.GroupSummary
 import io.reactivex.Observable
-import io.reactivex.subjects.BehaviorSubject
 
 class SelectedGroupHolder {
 
-    private val selectedGroupStream = BehaviorSubject.createDefault<Option<GroupSummary>>(Option.empty())
+    private val selectedGroupStream = BehaviorRelay.createDefault<Option<GroupSummary>>(Option.empty())
 
     fun setSelectedGroup(group: GroupSummary?) {
         val optionValue = Option.fromNullable(group)
-        selectedGroupStream.onNext(optionValue)
+        selectedGroupStream.accept(optionValue)
     }
 
     fun selectedGroup(): Observable<Option<GroupSummary>> {
--- app/src/main/java/im/vector/riotredesign/features/home/room/VisibleRoomHolder.kt
+++ app/src/main/java/im/vector/riotredesign/features/home/room/VisibleRoomHolder.kt
@@ -1,14 +1,15 @@
 package im.vector.riotredesign.features.home.room
 
+import com.jakewharton.rxrelay2.BehaviorRelay
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
 
 class VisibleRoomHolder {
 
-    private val visibleRoomStream = BehaviorSubject.create<String>()
+    private val visibleRoomStream = BehaviorRelay.create<String>()
 
     fun setVisibleRoom(roomId: String) {
-        visibleRoomStream.onNext(roomId)
+        visibleRoomStream.accept(roomId)
     }
 
     fun visibleRoom(): Observable<String> {
