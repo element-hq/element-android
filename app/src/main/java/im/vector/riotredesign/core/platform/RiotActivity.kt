2380499af22068ef1de858d1efd907ba47f9a108
ganfra <francoisg@matrix.org>
2019-01-17 18:20:01 +0100

    Login : quickly add homeserver url field

--- app/src/main/java/im/vector/riotredesign/core/platform/RiotActivity.kt
+++ app/src/main/java/im/vector/riotredesign/core/platform/RiotActivity.kt
@@ -1,5 +1,16 @@
 package im.vector.riotredesign.core.platform
 
 import com.airbnb.mvrx.BaseMvRxActivity
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
-abstract class RiotActivity : BaseMvRxActivity()
+abstract class RiotActivity : BaseMvRxActivity() {
+
+    private val uiDisposables = CompositeDisposable()
+
+    protected fun Disposable.disposeOnDestroy(): Disposable {
+        uiDisposables.add(this)
+        return this
+    }
+
+}
--- app/src/main/java/im/vector/riotredesign/features/login/LoginActivity.kt
+++ app/src/main/java/im/vector/riotredesign/features/login/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import arrow.core.Try
+import com.jakewharton.rxbinding2.widget.RxTextView
 import im.vector.matrix.android.api.Matrix
 import im.vector.matrix.android.api.MatrixCallback
 import im.vector.matrix.android.api.auth.data.HomeServerConnectionConfig
@@ -12,7 +14,14 @@ import im.vector.matrix.android.api.session.Session
 import im.vector.riotredesign.R
 import im.vector.riotredesign.core.platform.RiotActivity
 import im.vector.riotredesign.features.home.HomeActivity
+import io.reactivex.Observable
+import io.reactivex.functions.Function3
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_login.*
+
+private const val DEFAULT_HOME_SERVER_URI = "https://matrix.org"
+private const val DEFAULT_IDENTITY_SERVER_URI = "https://vector.im"
+private const val DEFAULT_ANTIVIRUS_SERVER_URI = "https://matrix.org"
 
 class LoginActivity : RiotActivity() {
 
@@ -21,19 +30,21 @@ class LoginActivity : RiotActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        authenticateButton.setOnClickListener { authenticate() }
+        setupAuthButton()
+        homeServerField.setText(DEFAULT_HOME_SERVER_URI)
     }
 
     private fun authenticate() {
-        val login = loginField.text.trim().toString()
-        val password = passwordField.text.trim().toString()
-        progressBar.visibility = View.VISIBLE
-        val homeServerConnectionConfig = HomeServerConnectionConfig.Builder()
-                .withHomeServerUri("https://matrix.org/")
-                .withIdentityServerUri("https://vector.im")
-                .withAntiVirusServerUri("https://matrix.org/")
-                .build()
+        val login = loginField.text?.trim().toString()
+        val password = passwordField.text?.trim().toString()
+        buildHomeServerConnectionConfig().fold(
+                { Toast.makeText(this@LoginActivity, "Authenticate failure: $it", Toast.LENGTH_LONG).show() },
+                { authenticateWith(it, login, password) }
+        )
+    }
 
+    private fun authenticateWith(homeServerConnectionConfig: HomeServerConnectionConfig, login: String, password: String) {
+        progressBar.visibility = View.VISIBLE
         authenticator.authenticate(homeServerConnectionConfig, login, password, object : MatrixCallback<Session> {
             override fun onSuccess(data: Session) {
                 Matrix.getInstance().currentSession = data
@@ -48,6 +59,31 @@ class LoginActivity : RiotActivity() {
         })
     }
 
+    private fun buildHomeServerConnectionConfig(): Try<HomeServerConnectionConfig> {
+        return Try {
+            val homeServerUri = homeServerField.text?.trim().toString()
+            HomeServerConnectionConfig.Builder()
+                    .withHomeServerUri(homeServerUri)
+                    .withIdentityServerUri(DEFAULT_IDENTITY_SERVER_URI)
+                    .withAntiVirusServerUri(DEFAULT_ANTIVIRUS_SERVER_URI)
+                    .build()
+        }
+    }
+
+    private fun setupAuthButton() {
+        Observable
+                .combineLatest(
+                        RxTextView.textChanges(loginField).map { it.trim().isNotEmpty() },
+                        RxTextView.textChanges(passwordField).map { it.trim().isNotEmpty() },
+                        RxTextView.textChanges(homeServerField).map { it.trim().isNotEmpty() },
+                        Function3<Boolean, Boolean, Boolean, Boolean> { isLoginNotEmpty, isPasswordNotEmpty, isHomeServerNotEmpty ->
+                            isLoginNotEmpty && isPasswordNotEmpty && isHomeServerNotEmpty
+                        }
+                )
+                .subscribeBy { authenticateButton.isEnabled = it }
+                .disposeOnDestroy()
+        authenticateButton.setOnClickListener { authenticate() }
+    }
 
     private fun goToHome() {
         val intent = HomeActivity.newIntent(this)
