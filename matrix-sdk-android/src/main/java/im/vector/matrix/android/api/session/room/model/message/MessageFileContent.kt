f9ca8f35bc621774773fabdda7ec311ae5614c83
ganfra <francois.ganard@gmail.com>
2019-01-23 16:25:51 +0100

    Fix MessageFileContent crash (no filename)

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/api/session/room/model/message/MessageFileContent.kt
+++ matrix-sdk-android/src/main/java/im/vector/matrix/android/api/session/room/model/message/MessageFileContent.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 data class MessageFileContent(
         @Json(name = "msgtype") override val type: String,
         @Json(name = "body") override val body: String,
-        @Json(name = "filename") val filename: String,
+        @Json(name = "filename") val filename: String? = null,
         @Json(name = "info") val info: FileInfo,
         @Json(name = "url") val url: String? = null
 ) : MessageContent
