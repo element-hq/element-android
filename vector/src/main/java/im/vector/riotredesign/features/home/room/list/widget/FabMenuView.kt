56f1c726b2dfaab9fab072744871bba2d5ddea73
Benoit Marty <benoit.marty@gmail.com>
2019-06-10 13:37:54 +0200

    FAB Motion WIP
    FAB Motion WIP
    
    FAB Motion WIP
    
    FAB Motion WIP

--- vector/src/main/java/im/vector/riotredesign/features/home/room/list/widget/FabMenuView.kt
+++ vector/src/main/java/im/vector/riotredesign/features/home/room/list/widget/FabMenuView.kt
@@ -18,55 +18,28 @@ package im.vector.riotredesign.features.home.room.list.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.isVisible
-import androidx.transition.ChangeTransform
-import androidx.transition.Transition
-import androidx.transition.TransitionManager
+import androidx.constraintlayout.motion.widget.MotionLayout
 import im.vector.riotredesign.R
-import im.vector.riotredesign.core.animations.ANIMATION_DURATION_SHORT
-import im.vector.riotredesign.core.animations.SimpleTransitionListener
-import im.vector.riotredesign.core.animations.VectorFullTransitionSet
 import im.vector.riotredesign.features.themes.ThemeUtils
-import kotlinx.android.synthetic.main.merge_fab_menu_view.view.*
+import kotlinx.android.synthetic.main.motion_fab_menu_merge.view.*
 
 class FabMenuView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
-                                            defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
+                                            defStyleAttr: Int = 0) : MotionLayout(context, attrs, defStyleAttr) {
 
     var listener: Listener? = null
 
-    private var isFabMenuOpened = false
-
     init {
-        inflate(context, R.layout.merge_fab_menu_view, this)
+        inflate(context, R.layout.motion_fab_menu_merge, this)
     }
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-
-        // Collapse
-        ConstraintSet().also {
-            it.clone(context, R.layout.constraint_set_fab_menu_close)
-            it.applyTo(this)
-        }
-
-        createRoomItemChat.isVisible = false
-        createRoomItemChatLabel.isVisible = false
-        createRoomItemGroup.isVisible = false
-        createRoomItemGroupLabel.isVisible = false
-        // Collapse end
 
         // Tint label background
         listOf(createRoomItemChatLabel, createRoomItemGroupLabel)
                 .forEach {
                     it.setBackgroundResource(ThemeUtils.getResourceId(context, R.drawable.vector_label_background_light))
                 }
-
-        createRoomButton.setOnClickListener {
-            toggleFabMenu()
-        }
 
         listOf(createRoomItemChat, createRoomItemChatLabel)
                 .forEach {
@@ -89,6 +62,9 @@ class FabMenuView @JvmOverloads constructor(context: Context, attrs: AttributeSe
     }
 
     fun show() {
+        // Ensure visibility is well restored
+        // FIXME It does not work, FAB is not visible, alpha is 0... And cannot set it to 1
+
         createRoomButton.show()
     }
 
@@ -96,61 +72,12 @@ class FabMenuView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         createRoomButton.hide()
     }
 
-    private fun openFabMenu() {
-        if (isFabMenuOpened) {
-            return
-        }
-
-        toggleFabMenu()
-    }
-
     private fun closeFabMenu() {
-        if (!isFabMenuOpened) {
-            return
-        }
-
-        toggleFabMenu()
-    }
-
-    private fun toggleFabMenu() {
-        isFabMenuOpened = !isFabMenuOpened
-
-        TransitionManager.beginDelayedTransition(parent as? ViewGroup ?: this,
-                VectorFullTransitionSet().apply {
-                    duration = ANIMATION_DURATION_SHORT
-                    ChangeTransform()
-                    addListener(object : SimpleTransitionListener() {
-                        override fun onTransitionEnd(transition: Transition) {
-                            // Hide the view after the transition for a better visual effect
-                            createRoomItemChat.isVisible = isFabMenuOpened
-                            createRoomItemChatLabel.isVisible = isFabMenuOpened
-                            createRoomItemGroup.isVisible = isFabMenuOpened
-                            createRoomItemGroupLabel.isVisible = isFabMenuOpened
-                        }
-                    })
-                })
-
-        if (isFabMenuOpened) {
-            // Animate manually the rotation for a better effect
-            createRoomButton.animate().setDuration(ANIMATION_DURATION_SHORT).rotation(135f)
-
-
-            ConstraintSet().also {
-                it.clone(context, R.layout.constraint_set_fab_menu_open)
-                it.applyTo(this)
-            }
-        } else {
-            createRoomButton.animate().setDuration(ANIMATION_DURATION_SHORT).rotation(0f)
-
-            ConstraintSet().also {
-                it.clone(context, R.layout.constraint_set_fab_menu_close)
-                it.applyTo(this)
-            }
-        }
+        transitionToStart()
     }
 
     fun onBackPressed(): Boolean {
-        if (isFabMenuOpened) {
+        if (currentState == R.id.constraint_set_fab_menu_open) {
             closeFabMenu()
             return true
         }
