1dbb02a80d2e1a2d2a3539048ddc0840743a55e4
ganfra <francoisg@matrix.org>
2019-08-08 17:51:06 +0200

    Read receipts: create custom view to use it wherever we want easily

--- vector/src/main/java/im/vector/riotx/features/home/room/detail/timeline/item/AbsMessageItem.kt
+++ vector/src/main/java/im/vector/riotx/features/home/room/detail/timeline/item/AbsMessageItem.kt
@@ -33,14 +33,13 @@ import com.airbnb.epoxy.EpoxyAttribute
 import im.vector.matrix.android.api.session.room.send.SendState
 import im.vector.riotx.R
 import im.vector.riotx.core.resources.ColorProvider
+import im.vector.riotx.core.ui.views.ReadReceiptsView
 import im.vector.riotx.core.utils.DebouncedClickListener
 import im.vector.riotx.core.utils.DimensionUtils.dpToPx
 import im.vector.riotx.features.home.AvatarRenderer
 import im.vector.riotx.features.home.room.detail.timeline.TimelineEventController
 import im.vector.riotx.features.reactions.widget.ReactionButton
 import im.vector.riotx.features.ui.getMessageTextColor
-
-private const val MAX_RECEIPT_DISPLAYED = 5
 
 abstract class AbsMessageItem<H : AbsMessageItem.Holder> : BaseEventItem<H>() {
 
@@ -125,28 +124,7 @@ abstract class AbsMessageItem<H : AbsMessageItem.Holder> : BaseEventItem<H>() {
             holder.memberNameView.setOnLongClickListener(null)
         }
 
-        if (informationData.readReceipts.isNotEmpty()) {
-            holder.readReceiptsView.isVisible = true
-            for (index in 0 until MAX_RECEIPT_DISPLAYED) {
-                val receiptData = informationData.readReceipts.getOrNull(index)
-                if (receiptData == null) {
-                    holder.receiptAvatars[index].isVisible = false
-                } else {
-                    holder.receiptAvatars[index].isVisible = true
-                    avatarRenderer.render(receiptData.avatarUrl, receiptData.userId, receiptData.displayName, holder.receiptAvatars[index])
-                }
-            }
-            if (informationData.readReceipts.size > MAX_RECEIPT_DISPLAYED) {
-                holder.receiptMoreView.isVisible = true
-                holder.receiptMoreView.text = holder.view.context.getString(
-                        R.string.x_plus, informationData.readReceipts.size - MAX_RECEIPT_DISPLAYED
-                )
-            } else {
-                holder.receiptMoreView.isVisible = false
-            }
-        } else {
-            holder.readReceiptsView.isVisible = false
-        }
+        holder.readReceiptsView.render(informationData.readReceipts, avatarRenderer)
 
         if (!shouldShowReactionAtBottom() || informationData.orderedReactionList.isNullOrEmpty()) {
             holder.reactionWrapper?.isVisible = false
@@ -198,17 +176,7 @@ abstract class AbsMessageItem<H : AbsMessageItem.Holder> : BaseEventItem<H>() {
         val avatarImageView by bind<ImageView>(R.id.messageAvatarImageView)
         val memberNameView by bind<TextView>(R.id.messageMemberNameView)
         val timeView by bind<TextView>(R.id.messageTimeView)
-        val readReceiptsView by bind<ViewGroup>(R.id.readReceiptsView)
-        val receiptAvatar1 by bind<ImageView>(R.id.message_avatar_receipt_1)
-        val receiptAvatar2 by bind<ImageView>(R.id.message_avatar_receipt_2)
-        val receiptAvatar3 by bind<ImageView>(R.id.message_avatar_receipt_3)
-        val receiptAvatar4 by bind<ImageView>(R.id.message_avatar_receipt_4)
-        val receiptAvatar5 by bind<ImageView>(R.id.message_avatar_receipt_5)
-        val receiptMoreView by bind<TextView>(R.id.message_more_than_expected)
-        val receiptAvatars: List<ImageView> by lazy {
-            listOf(receiptAvatar1, receiptAvatar2, receiptAvatar3, receiptAvatar4, receiptAvatar5)
-        }
-
+        val readReceiptsView by bind<ReadReceiptsView>(R.id.readReceiptsView)
         var reactionWrapper: ViewGroup? = null
         var reactionFlowHelper: Flow? = null
     }
--- vector/src/main/java/im/vector/riotx/features/home/room/detail/timeline/item/NoticeItem.kt
+++ vector/src/main/java/im/vector/riotx/features/home/room/detail/timeline/item/NoticeItem.kt
@@ -22,6 +22,7 @@ import android.widget.TextView
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
 import im.vector.riotx.R
+import im.vector.riotx.core.ui.views.ReadReceiptsView
 import im.vector.riotx.features.home.AvatarRenderer
 import im.vector.riotx.features.home.room.detail.timeline.TimelineEventController
 
@@ -55,6 +56,7 @@ abstract class NoticeItem : BaseEventItem<NoticeItem.Holder>() {
                 holder.avatarImageView
         )
         holder.view.setOnLongClickListener(longClickListener)
+        holder.readReceiptsView.render(informationData.readReceipts, avatarRenderer)
     }
 
     override fun getViewType() = STUB_ID
@@ -62,6 +64,7 @@ abstract class NoticeItem : BaseEventItem<NoticeItem.Holder>() {
     class Holder : BaseHolder(STUB_ID) {
         val avatarImageView by bind<ImageView>(R.id.itemNoticeAvatarView)
         val noticeTextView by bind<TextView>(R.id.itemNoticeTextView)
+        val readReceiptsView by bind<ReadReceiptsView>(R.id.readReceiptsView)
     }
 
     companion object {
