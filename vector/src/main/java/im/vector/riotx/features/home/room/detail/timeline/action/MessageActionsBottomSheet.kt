fce576e3a4c66632d4f5682547eaa0a752ff396a
Benoit Marty <benoitm@matrix.org>
2019-10-21 16:41:14 +0200

    Message action bottom sheet expanded

--- vector/src/main/java/im/vector/riotx/features/home/room/detail/timeline/action/MessageActionsBottomSheet.kt
+++ vector/src/main/java/im/vector/riotx/features/home/room/detail/timeline/action/MessageActionsBottomSheet.kt
@@ -45,6 +45,8 @@ class MessageActionsBottomSheet : VectorBaseBottomSheetDialogFragment(), Message
 
     private val viewModel: MessageActionsViewModel by fragmentViewModel(MessageActionsViewModel::class)
 
+    override val showExpanded = true
+
     private lateinit var actionHandlerModel: ActionsHandler
 
     override fun injectWith(screenComponent: ScreenComponent) {
