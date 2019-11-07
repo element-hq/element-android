241220ce1f3c11707b8dfed10429ffc7a1fcf900
Benoit Marty <benoitm@matrix.org>
2019-11-07 13:59:21 +0100

    remove unused import

--- vector/src/main/java/im/vector/riotx/features/reactions/EmojiReactionPickerActivity.kt
+++ vector/src/main/java/im/vector/riotx/features/reactions/EmojiReactionPickerActivity.kt
@@ -25,7 +25,6 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.SearchView
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
