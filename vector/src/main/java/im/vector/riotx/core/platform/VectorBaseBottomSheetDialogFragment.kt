6463f3439f90c234047ff3536b5c2d864a6221fb
ganfra <francois.ganard@gmail.com>
2019-11-12 15:11:52 +0100

    Update Mvrx to 1.3

--- vector/src/main/java/im/vector/riotx/core/platform/VectorBaseBottomSheetDialogFragment.kt
+++ vector/src/main/java/im/vector/riotx/core/platform/VectorBaseBottomSheetDialogFragment.kt
@@ -25,24 +25,22 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.airbnb.mvrx.MvRx
 import com.airbnb.mvrx.MvRxView
-import com.airbnb.mvrx.MvRxViewModelStore
+import com.airbnb.mvrx.MvRxViewId
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import im.vector.riotx.core.di.DaggerScreenComponent
 import im.vector.riotx.core.di.ScreenComponent
 import im.vector.riotx.core.utils.DimensionConverter
-import java.util.*
 
 /**
  * Add MvRx capabilities to bottomsheetdialog (like BaseMvRxFragment)
  */
 abstract class VectorBaseBottomSheetDialogFragment : BottomSheetDialogFragment(), MvRxView {
 
-    override val mvrxViewModelStore by lazy { MvRxViewModelStore(viewModelStore) }
-    private lateinit var mvrxPersistedViewId: String
+    private val mvrxViewIdProperty = MvRxViewId()
+    final override val mvrxViewId: String by mvrxViewIdProperty
     private lateinit var screenComponent: ScreenComponent
-    final override val mvrxViewId: String by lazy { mvrxPersistedViewId }
 
     /* ==========================================================================================
      * View model
@@ -78,10 +76,7 @@ abstract class VectorBaseBottomSheetDialogFragment : BottomSheetDialogFragment()
     protected open fun injectWith(screenComponent: ScreenComponent) = Unit
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        mvrxViewModelStore.restoreViewModels(this, savedInstanceState)
-        mvrxPersistedViewId = savedInstanceState?.getString(PERSISTED_VIEW_ID_KEY)
-                ?: this::class.java.simpleName + "_" + UUID.randomUUID().toString()
-
+        mvrxViewIdProperty.restoreFrom(savedInstanceState)
         super.onCreate(savedInstanceState)
     }
 
@@ -98,8 +93,7 @@ abstract class VectorBaseBottomSheetDialogFragment : BottomSheetDialogFragment()
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        mvrxViewModelStore.saveViewModels(outState)
-        outState.putString(PERSISTED_VIEW_ID_KEY, mvrxViewId)
+        mvrxViewIdProperty.saveTo(outState)
     }
 
     override fun onStart() {
@@ -121,5 +115,3 @@ abstract class VectorBaseBottomSheetDialogFragment : BottomSheetDialogFragment()
         arguments = args?.let { Bundle().apply { putParcelable(MvRx.KEY_ARG, it) } }
     }
 }
-
-private const val PERSISTED_VIEW_ID_KEY = "mvrx:bottomsheet_persisted_view_id"
