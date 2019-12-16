e2b4899b36941c25f2b649e66931aaf3aa9e1598
Benoit Marty <benoitm@matrix.org>
2019-12-16 15:21:24 +0100

    Internal review

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/session/room/alias/RoomAliasDescription.kt
+++ matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/session/room/alias/RoomAliasDescription.kt
@@ -20,7 +20,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class RoomAliasDescription(
+internal data class RoomAliasDescription(
         /**
          * The room ID for this alias.
          */
