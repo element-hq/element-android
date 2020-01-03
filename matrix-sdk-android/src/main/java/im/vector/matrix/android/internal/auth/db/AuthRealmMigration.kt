160927e7b53323d8bf74d379dacd191cfa7c7484
Benoit Marty <benoitm@matrix.org>
2020-01-03 14:25:19 +0100

    Split code into several methods

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/auth/db/AuthRealmMigration.kt
+++ matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/auth/db/AuthRealmMigration.kt
@@ -31,51 +31,55 @@ internal object AuthRealmMigration : RealmMigration {
     override fun migrate(realm: DynamicRealm, oldVersion: Long, newVersion: Long) {
         Timber.d("Migrating Auth Realm from $oldVersion to $newVersion")
 
-        if (oldVersion <= 0) {
-            Timber.d("Step 0 -> 1")
-            Timber.d("Create PendingSessionEntity")
+        if (oldVersion <= 0) migrateTo1(realm)
+        if (oldVersion <= 1) migrateTo2(realm)
+        if (oldVersion <= 2) migrateTo3(realm)
+    }
 
-            realm.schema.create("PendingSessionEntity")
-                    .addField(PendingSessionEntityFields.HOME_SERVER_CONNECTION_CONFIG_JSON, String::class.java)
-                    .setRequired(PendingSessionEntityFields.HOME_SERVER_CONNECTION_CONFIG_JSON, true)
-                    .addField(PendingSessionEntityFields.CLIENT_SECRET, String::class.java)
-                    .setRequired(PendingSessionEntityFields.CLIENT_SECRET, true)
-                    .addField(PendingSessionEntityFields.SEND_ATTEMPT, Integer::class.java)
-                    .setRequired(PendingSessionEntityFields.SEND_ATTEMPT, true)
-                    .addField(PendingSessionEntityFields.RESET_PASSWORD_DATA_JSON, String::class.java)
-                    .addField(PendingSessionEntityFields.CURRENT_SESSION, String::class.java)
-                    .addField(PendingSessionEntityFields.IS_REGISTRATION_STARTED, Boolean::class.java)
-                    .addField(PendingSessionEntityFields.CURRENT_THREE_PID_DATA_JSON, String::class.java)
-        }
+    private fun migrateTo1(realm: DynamicRealm) {
+        Timber.d("Step 0 -> 1")
+        Timber.d("Create PendingSessionEntity")
 
-        if (oldVersion <= 1) {
-            Timber.d("Step 1 -> 2")
-            Timber.d("Add boolean isTokenValid in SessionParamsEntity, with value true")
+        realm.schema.create("PendingSessionEntity")
+                .addField(PendingSessionEntityFields.HOME_SERVER_CONNECTION_CONFIG_JSON, String::class.java)
+                .setRequired(PendingSessionEntityFields.HOME_SERVER_CONNECTION_CONFIG_JSON, true)
+                .addField(PendingSessionEntityFields.CLIENT_SECRET, String::class.java)
+                .setRequired(PendingSessionEntityFields.CLIENT_SECRET, true)
+                .addField(PendingSessionEntityFields.SEND_ATTEMPT, Integer::class.java)
+                .setRequired(PendingSessionEntityFields.SEND_ATTEMPT, true)
+                .addField(PendingSessionEntityFields.RESET_PASSWORD_DATA_JSON, String::class.java)
+                .addField(PendingSessionEntityFields.CURRENT_SESSION, String::class.java)
+                .addField(PendingSessionEntityFields.IS_REGISTRATION_STARTED, Boolean::class.java)
+                .addField(PendingSessionEntityFields.CURRENT_THREE_PID_DATA_JSON, String::class.java)
+    }
 
-            realm.schema.get("SessionParamsEntity")
-                    ?.addField(SessionParamsEntityFields.IS_TOKEN_VALID, Boolean::class.java)
-                    ?.transform { it.set(SessionParamsEntityFields.IS_TOKEN_VALID, true) }
-        }
+    private fun migrateTo2(realm: DynamicRealm) {
+        Timber.d("Step 1 -> 2")
+        Timber.d("Add boolean isTokenValid in SessionParamsEntity, with value true")
 
-        if (oldVersion <= 2) {
-            Timber.d("Step 2 -> 3")
-            Timber.d("Update SessionParamsEntity primary key, to allow several sessions with the same userId")
+        realm.schema.get("SessionParamsEntity")
+                ?.addField(SessionParamsEntityFields.IS_TOKEN_VALID, Boolean::class.java)
+                ?.transform { it.set(SessionParamsEntityFields.IS_TOKEN_VALID, true) }
+    }
 
-            realm.schema.get("SessionParamsEntity")
-                    ?.removePrimaryKey()
-                    ?.addField(SessionParamsEntityFields.SESSION_ID, String::class.java)
-                    ?.setRequired(SessionParamsEntityFields.SESSION_ID, true)
-                    ?.transform {
-                        val userId = it.getString(SessionParamsEntityFields.USER_ID)
-                        val credentialsJson = it.getString(SessionParamsEntityFields.CREDENTIALS_JSON)
+    private fun migrateTo3(realm: DynamicRealm) {
+        Timber.d("Step 2 -> 3")
+        Timber.d("Update SessionParamsEntity primary key, to allow several sessions with the same userId")
 
-                        val credentials = MoshiProvider.providesMoshi()
-                                .adapter(Credentials::class.java)
-                                .fromJson(credentialsJson)
+        realm.schema.get("SessionParamsEntity")
+                ?.removePrimaryKey()
+                ?.addField(SessionParamsEntityFields.SESSION_ID, String::class.java)
+                ?.setRequired(SessionParamsEntityFields.SESSION_ID, true)
+                ?.transform {
+                    val userId = it.getString(SessionParamsEntityFields.USER_ID)
+                    val credentialsJson = it.getString(SessionParamsEntityFields.CREDENTIALS_JSON)
 
-                        it.set(SessionParamsEntityFields.SESSION_ID, createSessionId(userId, credentials?.deviceId))
-                    }
-                    ?.addPrimaryKey(SessionParamsEntityFields.SESSION_ID)
-        }
+                    val credentials = MoshiProvider.providesMoshi()
+                            .adapter(Credentials::class.java)
+                            .fromJson(credentialsJson)
+
+                    it.set(SessionParamsEntityFields.SESSION_ID, createSessionId(userId, credentials?.deviceId))
+                }
+                ?.addPrimaryKey(SessionParamsEntityFields.SESSION_ID)
     }
 }
