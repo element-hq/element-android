waylon531/riotX-android

Remove some of the realm listeners by processing directly the events

matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/session/room/EventRelationsAggregationProcessor.kt
@@ -16,7 +16,6 @@
 package im.vector.matrix.android.internal.session.room
 
 import com.zhuinden.monarchy.Monarchy
-import im.vector.matrix.android.api.session.crypto.CryptoService
 import im.vector.matrix.android.api.session.crypto.MXCryptoError
 import im.vector.matrix.android.api.session.events.model.AggregatedAnnotation
 import im.vector.matrix.android.api.session.events.model.Event
@@ -29,6 +28,7 @@ import im.vector.matrix.android.api.session.room.model.ReferencesAggregatedConte
 import im.vector.matrix.android.api.session.room.model.message.MessageContent
 import im.vector.matrix.android.api.session.room.model.message.MessageRelationContent
 import im.vector.matrix.android.api.session.room.model.relation.ReactionContent
+import im.vector.matrix.android.internal.crypto.DecryptEventTask
 import im.vector.matrix.android.internal.crypto.algorithms.olm.OlmDecryptionResult
 import im.vector.matrix.android.internal.crypto.model.event.EncryptedEventContent
 import im.vector.matrix.android.internal.database.mapper.ContentMapper
@@ -43,19 +43,12 @@ import im.vector.matrix.android.internal.database.model.TimelineEventEntity
 import im.vector.matrix.android.internal.database.query.create
 import im.vector.matrix.android.internal.database.query.getOrCreate
 import im.vector.matrix.android.internal.database.query.where
-import im.vector.matrix.android.internal.task.Task
+import im.vector.matrix.android.internal.di.UserId
+import im.vector.matrix.android.internal.session.sync.RoomEventsProcessor
 import im.vector.matrix.android.internal.util.awaitTransaction
 import io.realm.Realm
 import timber.log.Timber
 import javax.inject.Inject
-
-internal interface EventRelationsAggregationTask : Task<EventRelationsAggregationTask.Params, Unit> {
-
-    data class Params(
-            val events: List<Event>,
-            val userId: String
-    )
-}
 
 enum class VerificationState {
     REQUEST,
@@ -70,33 +63,27 @@ fun VerificationState.isCanceled(): Boolean {
 }
 
 /**
- * Called by EventRelationAggregationUpdater, when new events that can affect relations are inserted in base.
+ * Called when new events that can affect relations are synced.
  */
-internal class DefaultEventRelationsAggregationTask @Inject constructor(
+internal class EventRelationsAggregationProcessor @Inject constructor(
         private val monarchy: Monarchy,
-        private val cryptoService: CryptoService) : EventRelationsAggregationTask {
+        @UserId private val userId: String,
+        private val decryptEventTask: DecryptEventTask) : RoomEventsProcessor {
 
     // OPT OUT serer aggregation until API mature enough
     private val SHOULD_HANDLE_SERVER_AGREGGATION = false
 
-    override suspend fun execute(params: EventRelationsAggregationTask.Params) {
-        val events = params.events
-        val userId = params.userId
+    override suspend fun process(mode: RoomEventsProcessor.Mode, roomId: String, events: List<Event>) {
         monarchy.awaitTransaction { realm ->
-            Timber.v(">>> DefaultEventRelationsAggregationTask[${params.hashCode()}] called with ${events.size} events")
-            update(realm, events, userId)
-            Timber.v("<<< DefaultEventRelationsAggregationTask[${params.hashCode()}] finished")
+            Timber.v(">>> EventRelationsAggregationProcessor called with ${events.size} events")
+            update(realm, roomId, events, userId)
+            Timber.v("<<< EventRelationsAggregationProcessor finished")
         }
     }
 
-    private fun update(realm: Realm, events: List<Event>, userId: String) {
+    private suspend fun update(realm: Realm, roomId: String, events: List<Event>, userId: String) {
         events.forEach { event ->
             try { // Temporary catch, should be removed
-                val roomId = event.roomId
-                if (roomId == null) {
-                    Timber.w("Event has no room id ${event.eventId}")
-                    return@forEach
-                }
                 val isLocalEcho = LocalEcho.isLocalEchoId(event.eventId ?: "")
                 when (event.type) {
                     EventType.REACTION             -> {
@@ -198,10 +185,10 @@ internal class DefaultEventRelationsAggregationTask @Inject constructor(
         }
     }
 
-    private fun decryptIfNeeded(event: Event) {
+    private suspend fun decryptIfNeeded(event: Event) {
         if (event.mxDecryptionResult == null) {
             try {
-                val result = cryptoService.decryptEvent(event, event.roomId ?: "")
+                val result = decryptEventTask.decryptEvent(event, event.roomId ?: "")
                 event.mxDecryptionResult = OlmDecryptionResult(
                         payload = result.clearEvent,
                         senderKey = result.senderCurve25519Key,
@@ -504,4 +491,5 @@ internal class DefaultEventRelationsAggregationTask @Inject constructor(
         }
         return newState
     }
+
 }

element-hq/element-android

matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/session/room/EventRelationsAggregationProcessor.kt
@@ -47,19 +47,12 @@ import im.vector.matrix.android.internal.database.model.TimelineEventEntity
 import im.vector.matrix.android.internal.database.query.create
 import im.vector.matrix.android.internal.database.query.getOrCreate
 import im.vector.matrix.android.internal.database.query.where
-import im.vector.matrix.android.internal.task.Task
+import im.vector.matrix.android.internal.di.UserId
+import im.vector.matrix.android.internal.session.sync.RoomEventsProcessor
 import im.vector.matrix.android.internal.util.awaitTransaction
 import io.realm.Realm
 import timber.log.Timber
 import javax.inject.Inject
-
-internal interface EventRelationsAggregationTask : Task<EventRelationsAggregationTask.Params, Unit> {
-
-    data class Params(
-            val events: List<Event>,
-            val userId: String
-    )
-}
 
 enum class VerificationState {
     REQUEST,
@@ -89,33 +82,27 @@ private fun VerificationState?.toState(newState: VerificationState): Verificatio
 }
 
 /**
- * Called by EventRelationAggregationUpdater, when new events that can affect relations are inserted in base.
+ * Called when new events that can affect relations are synced.
  */
-internal class DefaultEventRelationsAggregationTask @Inject constructor(
+internal class EventRelationsAggregationProcessor @Inject constructor(
         private val monarchy: Monarchy,
-        private val cryptoService: CryptoService) : EventRelationsAggregationTask {
+        @UserId private val userId: String,
+        private val cryptoService: CryptoService) : RoomEventsProcessor {
 
     // OPT OUT serer aggregation until API mature enough
     private val SHOULD_HANDLE_SERVER_AGREGGATION = false
 
-    override suspend fun execute(params: EventRelationsAggregationTask.Params) {
-        val events = params.events
-        val userId = params.userId
+    override suspend fun process(mode: RoomEventsProcessor.Mode, roomId: String, events: List<Event>) {
         monarchy.awaitTransaction { realm ->
-            Timber.v(">>> DefaultEventRelationsAggregationTask[${params.hashCode()}] called with ${events.size} events")
-            update(realm, events, userId)
-            Timber.v("<<< DefaultEventRelationsAggregationTask[${params.hashCode()}] finished")
+            Timber.v(">>> EventRelationsAggregationProcessor called with ${events.size} events")
+            update(realm, roomId, events, userId)
+            Timber.v("<<< EventRelationsAggregationProcessor finished")
         }
     }
 
-    private fun update(realm: Realm, events: List<Event>, userId: String) {
+    private suspend fun update(realm: Realm, roomId: String, events: List<Event>, userId: String) {
         events.forEach { event ->
             try { // Temporary catch, should be removed
-                val roomId = event.roomId
-                if (roomId == null) {
-                    Timber.w("Event has no room id ${event.eventId}")
-                    return@forEach
-                }
                 val isLocalEcho = LocalEcho.isLocalEchoId(event.eventId ?: "")
                 when (event.type) {
                     EventType.REACTION             -> {
@@ -227,7 +214,7 @@ internal class DefaultEventRelationsAggregationTask @Inject constructor(
         }
     }
 
-    private fun decryptIfNeeded(event: Event) {
+    private suspend fun decryptIfNeeded(event: Event) {
         if (event.mxDecryptionResult == null) {
             try {
                 val result = cryptoService.decryptEvent(event, event.roomId ?: "")