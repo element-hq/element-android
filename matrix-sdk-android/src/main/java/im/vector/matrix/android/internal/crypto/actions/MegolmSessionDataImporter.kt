96af1957f95815dc4bf32950768ef88241ca86b5
Benoit Marty <benoitm@matrix.org>
2020-02-24 16:27:58 +0100

    Fix bug on progress step, found by integration test testBackupWithPassword()

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/crypto/actions/MegolmSessionDataImporter.kt
+++ matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/crypto/actions/MegolmSessionDataImporter.kt
@@ -83,7 +83,7 @@ internal class MegolmSessionDataImporter @Inject constructor(private val olmDevi
             }
 
             if (progressListener != null) {
-                val progress = 100 * cpt / totalNumbersOfKeys
+                val progress = 100 * (cpt + 1) / totalNumbersOfKeys
 
                 if (lastProgress != progress) {
                     lastProgress = progress
