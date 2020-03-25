6130a0a6543b1cdf99f2ce26872040c28a5f8d24
onurays <onurays@matrix.org>
2020-03-25 18:34:04 +0300

    Remove unused toString().

--- multipicker/src/main/java/im/vector/riotx/multipicker/entity/MultiPickerContactType.kt
+++ multipicker/src/main/java/im/vector/riotx/multipicker/entity/MultiPickerContactType.kt
@@ -21,12 +21,4 @@ data class MultiPickerContactType(
         val photoUri: String?,
         val phoneNumberList: List<String>,
         val emailList: List<String>
-) {
-    private val CONTACT_FORMAT = "Name: %s, Photo: %s, Phones: %s, Emails: %s"
-
-    override fun toString(): String {
-        val phoneNumberString = phoneNumberList.joinToString(separator = ", ", prefix = "[", postfix = "]")
-        val emailString = emailList.joinToString(separator = ", ", prefix = "[", postfix = "]")
-        return String.format(CONTACT_FORMAT, displayName, photoUri, phoneNumberString, emailString)
-    }
-}
+)
