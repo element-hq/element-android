6bbded1e65ab00a3035bbe2440e3afefa64ecf58
Nolan Darilek <nolan@thewordnerd.info>
2020-05-07 15:14:31 +0000

    Use string resource for generating ticker text.

--- vector/src/main/java/im/vector/riotx/features/notifications/NotificationDrawerManager.kt
+++ vector/src/main/java/im/vector/riotx/features/notifications/NotificationDrawerManager.kt
@@ -344,9 +344,9 @@ class NotificationDrawerManager @Inject constructor(private val context: Context
                     }
 
                     val tickerText = if (roomEventGroupInfo.isDirect) {
-                        String.format("%s: %s", events[0].senderName, events[0].description)
+                        stringProvider.getString(R.string.notification_ticker_text_dm, events[0].senderName, events[0].description)
                     } else {
-                        String.format("%s: %s %s", roomName, events[0].senderName, events[0].description)
+                        stringProvider.getString(R.string.notification_ticker_text_group, roomName, events[0].senderName, events[0].description)
                     }
 
                     val notification = notificationUtils.buildMessagesListNotification(
