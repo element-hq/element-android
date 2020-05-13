789bcc8d77373dd65b3b70c387a34d91375be130
Benoit Marty <benoitm@matrix.org>
2020-05-13 17:02:21 +0200

    Identity: Bugfix: do not fail when trying to disconnect the current identity server, if there is no token available

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/session/identity/IdentityDisconnectTask.kt
+++ matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/session/identity/IdentityDisconnectTask.kt
@@ -17,18 +17,30 @@
 package im.vector.matrix.android.internal.session.identity
 
 import im.vector.matrix.android.api.session.identity.IdentityServiceError
+import im.vector.matrix.android.internal.di.AuthenticatedIdentity
 import im.vector.matrix.android.internal.network.executeRequest
+import im.vector.matrix.android.internal.network.token.AccessTokenProvider
 import im.vector.matrix.android.internal.task.Task
+import timber.log.Timber
 import javax.inject.Inject
 
 internal interface IdentityDisconnectTask : Task<Unit, Unit>
 
 internal class DefaultIdentityDisconnectTask @Inject constructor(
-        private val identityApiProvider: IdentityApiProvider
+        private val identityApiProvider: IdentityApiProvider,
+        @AuthenticatedIdentity
+        private val accessTokenProvider: AccessTokenProvider
 ) : IdentityDisconnectTask {
 
     override suspend fun execute(params: Unit) {
         val identityAPI = identityApiProvider.identityApi ?: throw IdentityServiceError.NoIdentityServerConfigured
+
+        // Ensure we have a token.
+        // We can have an identity server configured, but no token yet.
+        if (accessTokenProvider.getToken() == null) {
+            Timber.d("No token to disconnect identity server.")
+            return
+        }
 
         executeRequest<Unit>(null) {
             apiCall = identityAPI.logout()
