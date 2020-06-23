0b5829ab6567f33516e9653765a3321f12950446
onurays <onurays@matrix.org>
2020-06-23 10:59:08 +0300

    Check if user have enough power level to change settings.

--- vector/src/main/java/im/vector/riotx/core/epoxy/profiles/ProfileActionItem.kt
+++ vector/src/main/java/im/vector/riotx/core/epoxy/profiles/ProfileActionItem.kt
@@ -71,7 +71,7 @@ abstract class ProfileActionItem : VectorEpoxyModel<ProfileActionItem.Holder>() 
     override fun bind(holder: Holder) {
         super.bind(holder)
         holder.view.setOnClickListener(listener)
-        if (listener == null) {
+        if (listener == null || !editable) {
             holder.view.isClickable = false
         }
         holder.title.text = title
