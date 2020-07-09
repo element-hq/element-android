f5284e8447e426dff2f8e094f9af2c8f2ef652aa
Onuray Sahin <onurays@matrix.org>
2020-07-09 15:15:27 +0300

    previously-riot url added to use in disclaimer dialog.

--- vector/src/main/java/im/vector/riotx/features/settings/VectorSettingsUrls.kt
+++ vector/src/main/java/im/vector/riotx/features/settings/VectorSettingsUrls.kt
@@ -21,5 +21,6 @@ object VectorSettingsUrls {
     const val COPYRIGHT = "https://element.io/copyright"
     const val TAC = "https://element.io/terms-of-service"
     const val PRIVACY_POLICY = "https://element.io/privacy"
+    const val DISCLAIMER_URL = "https://element.io/previously-riot"
     const val THIRD_PARTY_LICENSES = "file:///android_asset/open_source_licenses.html"
 }
