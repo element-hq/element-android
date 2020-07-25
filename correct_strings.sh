7c959176fd4b44ffaffe1fafd29fcbde4f65218d
SpiritCroc <dev@spiritcroc.de>
2020-07-25 10:27:00 +0200

    Update strings

--- correct_strings.sh
+++ correct_strings.sh
@@ -2,14 +2,18 @@
 
 mydir="$(dirname "$(realpath "$0")")"
 
+# Element -> SchildiChat
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
     sed -i 's|Element|SchildiChat|g' '{}' \;
+# Restore Element where it makes sense
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's|SchildiChat Web|Element Web|g' '{}' \;
+    sed -i 's/SchildiChat \(Web\|iOS\|Desktop\)/Element \1/g' '{}' \;
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
-    sed -i 's|SchildiChat iOS|Element iOS|g' '{}' \;
+    sed -i 's|SchildiChat Matrix Services|Element Matrix Services|g' '{}' \;
 find "$mydir/vector/src/main/res" -name strings.xml -exec \
     sed -i 's|\("use_latest_riot">.*\)SchildiChat\(.*</string>\)|\1Element\2|g' '{}' \;
+find "$mydir/vector/src/main/res" -name strings.xml -exec \
+    sed -i 's|\("use_other_session_content_description">.*\)SchildiChat\(.*SchildiChat.*</string>\)|\1SchildiChat/Element\2|' '{}' \;
 
 # Requires manual intervention for correct grammar
 sed -i 's|!nnen|wolpertinger|g' "$mydir/vector/src/main/res/values-de/strings.xml"
