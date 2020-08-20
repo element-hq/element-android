b090468109eda390bfbd3eb4d713c9a0698844a4
Benoit Marty <benoitm@matrix.org>
2020-08-20 14:12:40 +0200

    Logout after 3 wrong PIN codes

--- vector/src/main/java/im/vector/app/features/pin/PinFragment.kt
+++ vector/src/main/java/im/vector/app/features/pin/PinFragment.kt
@@ -29,6 +29,7 @@ import com.beautycoder.pflockscreen.fragments.PFLockScreenFragment
 import im.vector.app.R
 import im.vector.app.core.extensions.replaceFragment
 import im.vector.app.core.platform.VectorBaseFragment
+import im.vector.app.core.utils.toast
 import im.vector.app.features.MainActivity
 import im.vector.app.features.MainActivityArgs
 import kotlinx.android.parcel.Parcelize
@@ -61,7 +62,7 @@ class PinFragment @Inject constructor(
         val encodedPin = pinCodeStore.getEncodedPin() ?: return
         val authFragment = PFLockScreenFragment()
         val builder = PFFLockScreenConfiguration.Builder(requireContext())
-                .setUseBiometric(true)
+                .setUseBiometric(pinCodeStore.getRemainingBiometricsAttemptsNumber() > 0)
                 .setTitle(getString(R.string.auth_pin_confirm_to_disable_title))
                 .setClearCodeOnError(true)
                 .setMode(PFFLockScreenConfiguration.MODE_AUTH)
@@ -69,6 +70,7 @@ class PinFragment @Inject constructor(
         authFragment.setEncodedPinCode(encodedPin)
         authFragment.setLoginListener(object : PFLockScreenFragment.OnPFLockScreenLoginListener {
             override fun onPinLoginFailed() {
+                onWrongPin()
             }
 
             override fun onBiometricAuthSuccessful() {
@@ -80,6 +82,12 @@ class PinFragment @Inject constructor(
             }
 
             override fun onBiometricAuthLoginFailed() {
+                val remainingAttempts = pinCodeStore.onWrongBiometrics()
+                if (remainingAttempts <= 0) {
+                    // Disable Biometrics
+                    builder.setUseBiometric(false)
+                    authFragment.setConfiguration(builder.build())
+                }
             }
 
             override fun onCodeInputSuccessful() {
@@ -121,8 +129,12 @@ class PinFragment @Inject constructor(
     private fun showAuthFragment() {
         val encodedPin = pinCodeStore.getEncodedPin() ?: return
         val authFragment = PFLockScreenFragment()
+        val canUseBiometrics = pinCodeStore.getRemainingBiometricsAttemptsNumber() > 0
         val builder = PFFLockScreenConfiguration.Builder(requireContext())
                 .setUseBiometric(true)
+                .setAutoShowBiometric(true)
+                .setUseBiometric(canUseBiometrics)
+                .setAutoShowBiometric(canUseBiometrics)
                 .setTitle(getString(R.string.auth_pin_title))
                 .setLeftButton(getString(R.string.auth_pin_forgot))
                 .setClearCodeOnError(true)
@@ -134,22 +146,46 @@ class PinFragment @Inject constructor(
         }
         authFragment.setLoginListener(object : PFLockScreenFragment.OnPFLockScreenLoginListener {
             override fun onPinLoginFailed() {
+                onWrongPin()
             }
 
             override fun onBiometricAuthSuccessful() {
+                pinCodeStore.resetCounters()
                 vectorBaseActivity.setResult(Activity.RESULT_OK)
                 vectorBaseActivity.finish()
             }
 
             override fun onBiometricAuthLoginFailed() {
+                val remainingAttempts = pinCodeStore.onWrongBiometrics()
+                if (remainingAttempts <= 0) {
+                    // Disable Biometrics
+                    builder.setUseBiometric(false)
+                    authFragment.setConfiguration(builder.build())
+                }
             }
 
             override fun onCodeInputSuccessful() {
+                pinCodeStore.resetCounters()
                 vectorBaseActivity.setResult(Activity.RESULT_OK)
                 vectorBaseActivity.finish()
             }
         })
         replaceFragment(R.id.pinFragmentContainer, authFragment)
+    }
+
+    private fun onWrongPin() {
+        val remainingAttempts = pinCodeStore.onWrongPin()
+        when {
+            remainingAttempts > 1  ->
+                requireActivity().toast(resources.getQuantityString(R.plurals.wrong_pin_message_remaining_attempts, remainingAttempts, remainingAttempts))
+            remainingAttempts == 1 ->
+                requireActivity().toast(R.string.wrong_pin_message_last_remaining_attempt)
+            else                   -> {
+                requireActivity().toast(R.string.too_many_pin_failures)
+                // Logout
+                MainActivity.restartApp(requireActivity(), MainActivityArgs(clearCredentials = true))
+            }
+        }
     }
 
     private fun displayForgotPinWarningDialog() {
