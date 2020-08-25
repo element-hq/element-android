f5ea4fb6ac44f993eb49bced2d29cd3851e6ff65
Benoit Marty <benoit@matrix.org>
2020-08-25 16:15:42 +0200

    Refactor: Extract method to dedicated files

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/pushrules/ContainsDisplayNameCondition.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/pushrules/ContainsDisplayNameCondition.kt
@@ -20,7 +20,7 @@ import org.matrix.android.sdk.api.session.events.model.Event
 import org.matrix.android.sdk.api.session.events.model.EventType
 import org.matrix.android.sdk.api.session.events.model.toModel
 import org.matrix.android.sdk.api.session.room.model.message.MessageContent
-import timber.log.Timber
+import org.matrix.android.sdk.internal.util.caseInsensitiveFind
 
 class ContainsDisplayNameCondition : Condition {
 
@@ -43,31 +43,6 @@ class ContainsDisplayNameCondition : Condition {
             else              -> null
         } ?: return false
 
-        return caseInsensitiveFind(displayName, message.body)
-    }
-
-    companion object {
-        /**
-         * Returns whether a string contains an occurrence of another, as a standalone word, regardless of case.
-         *
-         * @param subString  the string to search for
-         * @param longString the string to search in
-         * @return whether a match was found
-         */
-        fun caseInsensitiveFind(subString: String, longString: String): Boolean {
-            // add sanity checks
-            if (subString.isEmpty() || longString.isEmpty()) {
-                return false
-            }
-
-            try {
-                val regex = Regex("(\\W|^)" + Regex.escape(subString) + "(\\W|$)", RegexOption.IGNORE_CASE)
-                return regex.containsMatchIn(longString)
-            } catch (e: Exception) {
-                Timber.e(e, "## caseInsensitiveFind() : failed")
-            }
-
-            return false
-        }
+        return message.body.caseInsensitiveFind(displayName)
     }
 }
--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/pushrules/EventMatchCondition.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/pushrules/EventMatchCondition.kt
@@ -18,6 +18,8 @@ package org.matrix.android.sdk.api.pushrules
 
 import org.matrix.android.sdk.api.session.events.model.Event
 import org.matrix.android.sdk.internal.di.MoshiProvider
+import org.matrix.android.sdk.internal.util.hasSpecialGlobChar
+import org.matrix.android.sdk.internal.util.simpleGlobToRegExp
 import timber.log.Timber
 
 class EventMatchCondition(
@@ -47,7 +49,7 @@ class EventMatchCondition(
         // Patterns with no special glob characters should be treated as having asterisks prepended
         // and appended when testing the condition.
         try {
-            val modPattern = if (hasSpecialGlobChar(pattern)) simpleGlobToRegExp(pattern) else simpleGlobToRegExp("*$pattern*")
+            val modPattern = if (pattern.hasSpecialGlobChar()) pattern.simpleGlobToRegExp() else "*$pattern*".simpleGlobToRegExp()
             val regex = Regex(modPattern, RegexOption.DOT_MATCHES_ALL)
             return regex.containsMatchIn(value)
         } catch (e: Throwable) {
@@ -75,28 +77,5 @@ class EventMatchCondition(
             }
         }
         return null
-    }
-
-    companion object {
-
-        private fun hasSpecialGlobChar(glob: String): Boolean {
-            return glob.contains("*") || glob.contains("?")
-        }
-
-        // Very simple glob to regexp converter
-        private fun simpleGlobToRegExp(glob: String): String {
-            var out = "" // "^"
-            for (element in glob) {
-                when (element) {
-                    '*'  -> out += ".*"
-                    '?'  -> out += '.'.toString()
-                    '.'  -> out += "\\."
-                    '\\' -> out += "\\\\"
-                    else -> out += element
-                }
-            }
-            out += "" // '$'.toString()
-            return out
-        }
     }
 }
