ef5c7d551105c16858caa94e0d322d757fab71b8
SpiritCroc <dev@spiritcroc.de>
2020-08-28 10:25:18 +0200

    Disable swipe-to-reply by default
    Change-Id: Ifbbb264e83634857a69870be52c5973dfc909e10

--- vector/src/main/java/im/vector/app/features/settings/VectorPreferences.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorPreferences.kt
@@ -279,7 +279,7 @@ class VectorPreferences @Inject constructor(private val context: Context) {
     }
 
     fun swipeToReplyIsEnabled(): Boolean {
-        return defaultPrefs.getBoolean(SETTINGS_LABS_ENABLE_SWIPE_TO_REPLY, true)
+        return defaultPrefs.getBoolean(SETTINGS_LABS_ENABLE_SWIPE_TO_REPLY, false)
     }
 
     fun mergeUTDinTimeline(): Boolean {
