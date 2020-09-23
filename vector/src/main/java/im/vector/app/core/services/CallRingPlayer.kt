4bdb4e9bce77198f35696aa66bc613f2becc1d2b
lomion0815 <lomion@aon.at>
2020-09-23 20:00:40 +0000

    Cleanup in CallRingPlayer.kt

--- vector/src/main/java/im/vector/app/core/services/CallRingPlayer.kt
+++ vector/src/main/java/im/vector/app/core/services/CallRingPlayer.kt
@@ -17,9 +17,6 @@
 package im.vector.app.core.services
 
 import android.content.Context
-import android.media.AudioAttributes
-import android.media.AudioManager
-import android.media.MediaPlayer
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri 
@@ -33,71 +30,16 @@ class CallRingPlayer(
 ) {
 
     private val applicationContext = context.applicationContext
-
-    private var player: MediaPlayer? = null
-    
     private var r: Ringtone? = null
 
     fun start() {
-//        val audioManager = applicationContext.getSystemService<AudioManager>()!!
-//        player?.release()
-//        player = createPlayer()
         val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
         r = RingtoneManager.getRingtone(applicationContext, notification)
+        Timber.v("## VOIP Starting ringing")
         r?.play()
-
-        // Check if sound is enabled
- /*       val ringerMode = audioManager.ringerMode
-        if (player != null && ringerMode == AudioManager.RINGER_MODE_NORMAL) {
-            try {
-                if (player?.isPlaying == false) {
-                    player?.start()
-                    Timber.v("## VOIP Starting ringing")
-                } else {
-                    Timber.v("## VOIP already playing")
-                }
-            } catch (failure: Throwable) {
-                Timber.e(failure, "## VOIP Failed to start ringing")
-                player = null
-            }
-        } else {
-            Timber.v("## VOIP Can't play $player ode $ringerMode")
-        }*/
     }
 
     fun stop() {
-        player?.release()
-        player = null
         r?.stop()
-    }
-
-    private fun createPlayer(): MediaPlayer? {
-        try {
-            val mediaPlayer = MediaPlayer.create(applicationContext, R.raw.ring)
-
-            mediaPlayer.setOnErrorListener(MediaPlayerErrorListener())
-            mediaPlayer.isLooping = true
-            if (Build.VERSION.SDK_INT <= 21) {
-                @Suppress("DEPRECATION")
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_RING)
-            } else {
-                mediaPlayer.setAudioAttributes(AudioAttributes.Builder()
-                        .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                        .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION_SIGNALLING)
-                        .build())
-            }
-            return mediaPlayer
-        } catch (failure: Throwable) {
-            Timber.e(failure, "Failed to create Call ring player")
-            return null
-        }
-    }
-
-    inner class MediaPlayerErrorListener : MediaPlayer.OnErrorListener {
-        override fun onError(mp: MediaPlayer, what: Int, extra: Int): Boolean {
-            Timber.w("onError($mp, $what, $extra")
-            player = null
-            return false
-        }
     }
 }
