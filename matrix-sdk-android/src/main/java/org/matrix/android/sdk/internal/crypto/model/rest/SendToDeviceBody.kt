b12c4f854c63180a57944be3dd2223877e4e5c4e
Valere <valeref@matrix.org>
2020-10-09 15:40:30 +0200

    Fix / Moshi Cannot serialize kotlin

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/crypto/model/rest/SendToDeviceBody.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/crypto/model/rest/SendToDeviceBody.kt
@@ -16,6 +16,9 @@
 
 package org.matrix.android.sdk.internal.crypto.model.rest
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 internal data class SendToDeviceBody(
         /**
          * `Any` should implement [SendToDeviceObject], but we cannot use interface here because of Json serialization
