element-hq/element-android

commit 65716c0ee8838744cee86c87f1080071125297e0

Don't restart activity if inactive theme is changed
Change-Id: I1241b419cf588606af7e465bb1ac8285a026b944

vector/src/main/java/im/vector/app/features/settings/VectorSettingsPreferencesFragment.kt
@@ -56,9 +56,11 @@ class VectorSettingsPreferencesFragment @Inject constructor(
         val darkThemePref = findPreference<VectorListPreference>(ThemeUtils.APPLICATION_DARK_THEME_KEY)!!
         lightThemePref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
             if (newValue is String) {
-                ThemeUtils.setApplicationLightTheme(requireContext().applicationContext, newValue)
-                // Restart the Activity
-                activity?.restart()
+                if (!ThemeUtils.shouldUseDarkTheme(requireContext())) {
+                    ThemeUtils.setApplicationLightTheme(requireContext().applicationContext, newValue)
+                    // Restart the Activity
+                    activity?.restart()
+                }
                 true
             } else {
                 false
@@ -68,12 +70,10 @@ class VectorSettingsPreferencesFragment @Inject constructor(
             lightThemePref.title = getString(R.string.settings_light_theme)
             darkThemePref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
                 if (newValue is String) {
-                    ThemeUtils.setApplicationDarkTheme(requireContext().applicationContext, newValue)
-                    // Restart the Activity
-                    activity?.let {
-                        // Note: recreate does not apply the color correctly
-                        it.startActivity(it.intent)
-                        it.finish()
+                    if (ThemeUtils.shouldUseDarkTheme(requireContext())) {
+                        ThemeUtils.setApplicationDarkTheme(requireContext().applicationContext, newValue)
+                        // Restart the Activity
+                        activity?.restart()
                     }
                     true
                 } else {

commit 863f07cbe4458e96098430d7ebad99dcd6a16569

Fix changing inactive theme
Bug introduced in commit 65716c0ee8838744cee86c87f1080071125297e0.

Fixes:
- Change inactive theme
- Switch to inactive theme -> still the old theme setting

Change-Id: I1b56144ffdb5e0f33efa67702e8aa05065285488

vector/src/main/java/im/vector/app/features/settings/VectorSettingsPreferencesFragment.kt
@@ -56,8 +56,8 @@ class VectorSettingsPreferencesFragment @Inject constructor(
         val darkThemePref = findPreference<VectorListPreference>(ThemeUtils.APPLICATION_DARK_THEME_KEY)!!
         lightThemePref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
             if (newValue is String) {
+                ThemeUtils.setApplicationLightTheme(requireContext().applicationContext, newValue)
                 if (!ThemeUtils.shouldUseDarkTheme(requireContext())) {
-                    ThemeUtils.setApplicationLightTheme(requireContext().applicationContext, newValue)
                     // Restart the Activity
                     activity?.restart()
                 }
@@ -70,8 +70,8 @@ class VectorSettingsPreferencesFragment @Inject constructor(
             lightThemePref.title = getString(R.string.settings_light_theme)
             darkThemePref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
                 if (newValue is String) {
+                    ThemeUtils.setApplicationDarkTheme(requireContext().applicationContext, newValue)
                     if (ThemeUtils.shouldUseDarkTheme(requireContext())) {
-                        ThemeUtils.setApplicationDarkTheme(requireContext().applicationContext, newValue)
                         // Restart the Activity
                         activity?.restart()
                     }