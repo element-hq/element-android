0d93105bcd140ed9940ec6beb5db0b31ac504084
Benoit Marty <benoit@matrix.org>
2020-11-20 15:29:35 +0100

    Rended m.room.canonical_alias event in the timeline, considering alt_aliases (#2428)

--- vector/src/main/java/im/vector/app/features/home/room/detail/timeline/format/NoticeEventFormatter.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/timeline/format/NoticeEventFormatter.kt
@@ -465,21 +465,76 @@ class NoticeEventFormatter @Inject constructor(
 
     private fun formatRoomCanonicalAliasEvent(event: Event, senderName: String?): String? {
         val eventContent: RoomCanonicalAliasContent? = event.getClearContent().toModel()
-        val canonicalAlias = eventContent?.canonicalAlias
-        return canonicalAlias
-                ?.takeIf { it.isNotBlank() }
-                ?.let {
-                    if (event.isSentByCurrentUser()) {
-                        sp.getString(R.string.notice_room_canonical_alias_set_by_you, it)
-                    } else {
-                        sp.getString(R.string.notice_room_canonical_alias_set, senderName, it)
-                    }
+        val prevContent: RoomCanonicalAliasContent? = event.resolvedPrevContent().toModel()
+        val canonicalAlias = eventContent?.canonicalAlias?.takeIf { it.isNotEmpty() }
+        val prevCanonicalAlias = prevContent?.canonicalAlias?.takeIf { it.isNotEmpty() }
+        val altAliases = eventContent?.alternativeAliases.orEmpty()
+        val prevAltAliases = prevContent?.alternativeAliases.orEmpty()
+        val added = altAliases - prevAltAliases
+        val removed = prevAltAliases - altAliases
+
+        return if (added.isEmpty() && removed.isEmpty() && canonicalAlias == prevCanonicalAlias) {
+            // in case there is no difference between the two events say something as we can't simply hide the event from here
+            if (event.isSentByCurrentUser()) {
+                sp.getString(R.string.notice_room_canonical_alias_no_change_by_you)
+            } else {
+                sp.getString(R.string.notice_room_canonical_alias_no_change, senderName)
+            }
+        } else if (added.isEmpty() && removed.isEmpty()) {
+            // Canonical has changed
+            if (canonicalAlias != null) {
+                if (event.isSentByCurrentUser()) {
+                    sp.getString(R.string.notice_room_canonical_alias_set_by_you, canonicalAlias)
+                } else {
+                    sp.getString(R.string.notice_room_canonical_alias_set, senderName, canonicalAlias)
                 }
-                ?: if (event.isSentByCurrentUser()) {
+            } else {
+                if (event.isSentByCurrentUser()) {
                     sp.getString(R.string.notice_room_canonical_alias_unset_by_you)
                 } else {
                     sp.getString(R.string.notice_room_canonical_alias_unset, senderName)
                 }
+            }
+        } else if (added.isEmpty()) {
+            if (canonicalAlias == prevCanonicalAlias) {
+                // Some alternative has been removed
+                if (event.isSentByCurrentUser()) {
+                    sp.getQuantityString(R.plurals.notice_room_canonical_alias_alternative_removed_by_you, removed.size, removed.joinToString())
+                } else {
+                    sp.getQuantityString(R.plurals.notice_room_canonical_alias_alternative_removed, removed.size, senderName, removed.joinToString())
+                }
+            } else {
+                // Main and removed
+                if (event.isSentByCurrentUser()) {
+                    sp.getString(R.string.notice_room_canonical_alias_main_and_alternative_changed_by_you)
+                } else {
+                    sp.getString(R.string.notice_room_canonical_alias_main_and_alternative_changed, senderName)
+                }
+            }
+        } else if (removed.isEmpty()) {
+            if (canonicalAlias == prevCanonicalAlias) {
+                // Some alternative has been added
+                if (event.isSentByCurrentUser()) {
+                    sp.getQuantityString(R.plurals.notice_room_canonical_alias_alternative_added_by_you, added.size, added.joinToString())
+                } else {
+                    sp.getQuantityString(R.plurals.notice_room_canonical_alias_alternative_added, added.size, senderName, added.joinToString())
+                }
+            } else {
+                // Main and added
+                if (event.isSentByCurrentUser()) {
+                    sp.getString(R.string.notice_room_canonical_alias_main_and_alternative_changed_by_you)
+                } else {
+                    sp.getString(R.string.notice_room_canonical_alias_main_and_alternative_changed, senderName)
+                }
+            }
+        } else {
+            // Alternative added and removed
+            if (event.isSentByCurrentUser()) {
+                sp.getString(R.string.notice_room_canonical_alias_alternative_changed_by_you)
+            } else {
+                sp.getString(R.string.notice_room_canonical_alias_alternative_changed, senderName)
+            }
+        }
     }
 
     private fun formatRoomGuestAccessEvent(event: Event, senderName: String?, rs: RoomSummary?): String? {
