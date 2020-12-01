c6ba2960287d51344b469be9529dc39e165b7a78
Benoit Marty <benoit@matrix.org>
2020-12-01 14:48:10 +0100

    Create an extension to apply the fix at several places

--- vector/src/main/java/im/vector/app/features/roomdirectory/createroom/RoomAliasEditItem.kt
+++ vector/src/main/java/im/vector/app/features/roomdirectory/createroom/RoomAliasEditItem.kt
@@ -27,6 +27,7 @@ import com.google.android.material.textfield.TextInputLayout
 import im.vector.app.R
 import im.vector.app.core.epoxy.VectorEpoxyHolder
 import im.vector.app.core.epoxy.VectorEpoxyModel
+import im.vector.app.core.extensions.setTextSafe
 import im.vector.app.core.platform.SimpleTextWatcher
 
 @EpoxyModelClass(layout = R.layout.item_room_alias_text_input)
@@ -62,9 +63,7 @@ abstract class RoomAliasEditItem : VectorEpoxyModel<RoomAliasEditItem.Holder>() 
         holder.textInputLayout.error = errorMessage
 
         // Update only if text is different and value is not null
-        if (value != null && holder.textInputEditText.text.toString() != value) {
-            holder.textInputEditText.setText(value)
-        }
+        holder.textInputEditText.setTextSafe(value)
         holder.textInputEditText.isEnabled = enabled
         holder.textInputEditText.addTextChangedListener(onTextChangeListener)
         holder.homeServerText.text = homeServer
