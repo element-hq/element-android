0c037184f857fc67a38c91022d5b5d6e971c4ce5
Benoit Marty <benoit@matrix.org>
2020-12-03 13:46:25 +0100

    Create a MediaService to handle UrlPreview request - WIP

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/media/MediaAPI.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/media/MediaAPI.kt
@@ -16,9 +16,11 @@
 
 package org.matrix.android.sdk.internal.session.media
 
+import org.matrix.android.sdk.api.util.JsonDict
 import org.matrix.android.sdk.internal.network.NetworkConstants
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 internal interface MediaAPI {
     /**
@@ -27,4 +29,15 @@ internal interface MediaAPI {
      */
     @GET(NetworkConstants.URI_API_MEDIA_PREFIX_PATH_R0 + "config")
     fun getMediaConfig(): Call<GetMediaConfigResult>
+
+    /**
+     * Get information about a URL for the client. Typically this is called when a client
+     * sees a URL in a message and wants to render a preview for the user.
+     * Ref: https://matrix.org/docs/spec/client_server/r0.6.1#get-matrix-media-r0-preview-url
+     * @param url Required. The URL to get a preview of.
+     * @param ts The preferred point in time to return a preview for. The server may return a newer version
+     * if it does not have the requested version available.
+     */
+    @GET(NetworkConstants.URI_API_MEDIA_PREFIX_PATH_R0 + "preview_url")
+    fun getPreviewUrlData(@Query("url") url: String, @Query("ts") ts: Int?): Call<JsonDict>
 }
