2542247d6a74437d79f922f898180cc44152ba10
SpiritCroc <dev@spiritcroc.de>
2020-12-24 12:21:48 +0100

    Update link to privacy policy
    Change-Id: I8e69b0b3b8a6a68bcc80c871988cf6edf4af8eb9

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
@@ -20,7 +20,7 @@ object VectorSettingsUrls {
 
     const val COPYRIGHT = "https://element.io/copyright"
     const val TAC = "https://element.io/terms-of-service"
-    const val PRIVACY_POLICY = "https://element.io/privacy"
+    const val PRIVACY_POLICY = "https://schildi.chat/android/privacy"
     const val DISCLAIMER_URL = "https://element.io/previously-riot"
     const val THIRD_PARTY_LICENSES = "file:///android_asset/open_source_licenses.html"
 }
