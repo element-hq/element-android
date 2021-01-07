a6f909b942538f2549da847492b2e9b5818133ab
Benoit Marty <benoit@matrix.org>
2021-01-07 11:04:11 +0100

    Add Javadoc

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/model/PowerLevelsContent.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/model/PowerLevelsContent.kt
@@ -25,15 +25,45 @@ import org.matrix.android.sdk.api.session.room.powerlevels.Role
  */
 @JsonClass(generateAdapter = true)
 data class PowerLevelsContent(
+        /**
+         * The level required to ban a user. Defaults to 50 if unspecified.
+         */
         @Json(name = "ban") val ban: Int = Role.Moderator.value,
+        /**
+         * The level required to kick a user. Defaults to 50 if unspecified.
+         */
         @Json(name = "kick") val kick: Int = Role.Moderator.value,
+        /**
+         * The level required to invite a user. Defaults to 50 if unspecified.
+         */
         @Json(name = "invite") val invite: Int = Role.Moderator.value,
+        /**
+         * The level required to redact an event. Defaults to 50 if unspecified.
+         */
         @Json(name = "redact") val redact: Int = Role.Moderator.value,
+        /**
+         * The default level required to send message events. Can be overridden by the events key. Defaults to 0 if unspecified.
+         */
         @Json(name = "events_default") val eventsDefault: Int = Role.Default.value,
+        /**
+         * The level required to send specific event types. This is a mapping from event type to power level required.
+         */
         @Json(name = "events") val events: Map<String, Int> = emptyMap(),
+        /**
+         * The default power level for every user in the room, unless their user_id is mentioned in the users key. Defaults to 0 if unspecified.
+         */
         @Json(name = "users_default") val usersDefault: Int = Role.Default.value,
+        /**
+         * The power levels for specific users. This is a mapping from user_id to power level for that user.
+         */
         @Json(name = "users") val users: Map<String, Int> = emptyMap(),
+        /**
+         * The default level required to send state events. Can be overridden by the events key. Defaults to 50 if unspecified.
+         */
         @Json(name = "state_default") val stateDefault: Int = Role.Moderator.value,
+        /**
+         * The power level requirements for specific notification types. This is a mapping from key to power level for that notifications key.
+         */
         @Json(name = "notifications") val notifications: Map<String, Any> = emptyMap()
 ) {
     /**
@@ -71,6 +101,9 @@ data class PowerLevelsContent(
     }
 
     companion object {
+        /**
+         * Key to use for content.notifications and get the level required to trigger an @room notification. Defaults to 50 if unspecified.
+         */
         const val NOTIFICATIONS_ROOM_KEY = "room"
     }
 }
