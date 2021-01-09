4a11802b65564eced4773c3994ef76517cc50555
dkanada <dkanada@users.noreply.github.com>
2021-01-09 23:23:17 +0900

    store cache for WidgetWebView between loads

--- vector/src/main/java/im/vector/app/features/widgets/webview/WidgetWebView.kt
+++ vector/src/main/java/im/vector/app/features/widgets/webview/WidgetWebView.kt
@@ -36,10 +36,6 @@ fun WebView.setupForWidget(webViewEventListener: WebViewEventListener) {
     // clear caches
     clearHistory()
     clearFormData()
-    clearCache(true)
-
-    // does not cache the data
-    settings.cacheMode = WebSettings.LOAD_NO_CACHE
 
     // Enable Javascript
     settings.javaScriptEnabled = true
@@ -78,8 +74,6 @@ fun WebView.clearAfterWidget() {
     (parent as? ViewGroup)?.removeAllViews()
     webChromeClient = null
     clearHistory()
-    // NOTE: clears RAM cache, if you pass true, it will also clear the disk cache.
-    clearCache(true)
     // Loading a blank page is optional, but will ensure that the WebView isn't doing anything when you destroy it.
     loadUrl("about:blank")
     removeAllViews()
