0930253032d856fb68f13a7ba49944b19ef17fd5
Valere <valeref@matrix.org>
2021-01-12 11:40:09 +0100

    Basic space join / use tmp msc id / db model update

--- vector/src/main/java/im/vector/app/features/spaces/SpaceListFragment.kt
+++ vector/src/main/java/im/vector/app/features/spaces/SpaceListFragment.kt
@@ -1,21 +1,20 @@
 /*
- * Copyright 2019 New Vector Ltd
+ * Copyright (c) 2021 New Vector Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-package im.vector.app.features.grouplist
+package im.vector.app.features.spaces
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -33,9 +32,6 @@ import im.vector.app.core.platform.VectorBaseFragment
 import im.vector.app.databinding.FragmentGroupListBinding
 import im.vector.app.features.home.HomeActivitySharedAction
 import im.vector.app.features.home.HomeSharedActionViewModel
-import im.vector.app.features.spaces.SpaceListAction
-import im.vector.app.features.spaces.SpaceListViewEvents
-import im.vector.app.features.spaces.SpacesListViewModel
 import org.matrix.android.sdk.api.session.space.SpaceSummary
 import javax.inject.Inject
 
