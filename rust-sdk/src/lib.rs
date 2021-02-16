4589b882c051cdfd83a6c408b57d74e1ae2c0ba7
Damir Jelić <poljar@termina.org.uk>
2021-02-16 11:18:31 +0100

    rust: Add support to forward rust logs to the kotlin side

--- rust-sdk/src/lib.rs
+++ rust-sdk/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{BTreeMap, HashMap},
     convert::TryFrom,
+    sync::{Arc, Mutex},
     time::Duration,
 };
 
@@ -25,6 +26,55 @@ use matrix_sdk_crypto::{
     store::CryptoStoreError as InnerStoreError, IncomingResponse, OlmError,
     OlmMachine as InnerMachine, OutgoingRequest, ToDeviceRequest,
 };
+use tracing_subscriber::{fmt::MakeWriter, EnvFilter};
+
+pub trait Logger: Send {
+    fn log(&self, log_line: String);
+}
+
+impl std::io::Write for LoggerWrapper {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let data = String::from_utf8_lossy(buf).to_string();
+        self.inner.lock().unwrap().log(data);
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
+impl MakeWriter for LoggerWrapper {
+    type Writer = LoggerWrapper;
+
+    fn make_writer(&self) -> Self::Writer {
+        self.clone()
+    }
+}
+
+#[derive(Clone)]
+pub struct LoggerWrapper {
+    inner: Arc<Mutex<Box<dyn Logger>>>,
+}
+
+pub fn set_logger(logger: Box<dyn Logger>) {
+    let logger = LoggerWrapper {
+        inner: Arc::new(Mutex::new(logger)),
+    };
+
+    let filter = EnvFilter::from_default_env().add_directive(
+        "matrix_sdk_crypto=trace"
+            .parse()
+            .expect("Can't parse logging filter directive"),
+    );
+
+    let _ = tracing_subscriber::fmt()
+        .with_writer(logger)
+        .with_env_filter(filter)
+        .without_time()
+        .try_init();
+}
 
 pub struct OlmMachine {
     inner: InnerMachine,
@@ -205,10 +255,7 @@ impl OlmMachine {
 
         Ok(OlmMachine {
             inner: block_on(InnerMachine::new_with_default_store(
-                &user_id,
-                device_id,
-                path,
-                Some("DEFAULT_PASSPHRASE"),
+                &user_id, device_id, path, None,
             ))?,
             runtime: Runtime::new().unwrap(),
         })
