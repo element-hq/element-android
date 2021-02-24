dca123c0082957dacd735572376d5295405b3ad3
Valere <valeref@matrix.org>
2021-02-25 00:17:27 +0100

    Support retry after M_LIMIT_EXCEEDED

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/task/Task.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/task/Task.kt
@@ -16,7 +16,29 @@
 
 package org.matrix.android.sdk.internal.task
 
+import kotlinx.coroutines.delay
+import org.matrix.android.sdk.api.failure.Failure
+import org.matrix.android.sdk.api.failure.shouldBeRetried
+import timber.log.Timber
+
 internal interface Task<PARAMS, RESULT> {
 
     suspend fun execute(params: PARAMS): RESULT
+
+    suspend fun executeRetry(params: PARAMS, remainingRetry: Int) : RESULT {
+        return try {
+            execute(params)
+        } catch (failure: Throwable) {
+            if (failure.shouldBeRetried() && remainingRetry > 0) {
+                Timber.d(failure, "## TASK: Retriable error")
+                if (failure is Failure.ServerError) {
+                    val waitTime = failure.error.retryAfterMillis ?: 0L
+                    Timber.d(failure, "## TASK: Quota wait time $waitTime")
+                    delay(waitTime + 100)
+                }
+                return executeRetry(params, remainingRetry - 1)
+            }
+            throw failure
+        }
+    }
 }
