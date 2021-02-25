167872e10164b8f6b834fc05e3934b48a5c8a0e9
yostyle <yoanp@element.io>
2021-02-25 12:20:25 +0100

    Tchap flavors

---
+++ vector/src/withpinning/java/fr/gouv/tchap/config/PinningConfiguration.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.gouv.tchap.config
+
+// Pinning is Enabled
+const val ENABLE_CERTIFICATE_PINNING = true
+
+// Put the list of fingerprint here.
+val CERTIFICATE_FINGERPRINT_LIST = listOf<String>("90:45:FA:C4:7B:B4:8D:62:50:5F:19:EC:4E:EB:95:30:A2:B7:82:8D:E1:B9:CD:DB:18:4E:A4:93:DE:70:F3:B1",
+        "EC:B2:DB:C3:BE:0A:95:AE:D5:EA:D9:AE:66:9A:E6:07:62:E5:7D:C5:71:35:AB:45:D4:24:87:9F:FB:F9:82:C1",
+        "2B:C6:54:20:3E:67:87:97:14:7C:24:9E:4B:E4:49:98:59:01:41:7C:07:DB:7D:11:92:DF:A5:D7:BE:6D:FF:19",
+        "4D:CB:28:2D:A1:13:6D:B1:B0:53:70:13:B6:66:58:A5:D1:72:AE:BA:54:6B:92:C6:0B:57:74:90:5E:4B:F7:91",
+        "1A:34:DF:9C:2D:BB:2B:90:CB:D3:0B:D1:F9:AB:A1:55:D6:EB:B4:01:39:AB:AE:CB:7E:1F:D9:53:F0:C1:F7:33",
+        "9A:03:F4:B1:83:7B:BF:F0:78:05:A5:D2:77:03:1F:0B:AA:37:2A:74:4B:D3:74:8B:64:95:AD:34:E7:D5:3E:44",
+        "24:E3:8A:BB:41:7D:12:A5:D0:75:8E:7B:8A:69:CA:61:FF:27:20:EF:38:B2:65:84:AC:66:D3:C6:38:41:30:6A",
+        "E7:5A:12:8B:BB:56:B9:DE:11:DF:3F:E7:59:1A:4F:22:B6:11:08:E1:D0:80:0D:22:9E:E5:7A:A2:D2:C8:EE:21",
+        "EB:EB:A9:7F:0D:AA:43:7B:A9:0F:9A:DA:7D:01:ED:C7:7F:3B:5C:8F:A4:84:36:71:69:00:89:34:8A:58:CB:4F",
+        "BB:9F:CD:BD:88:F6:F4:69:C5:59:D9:0C:B0:22:C5:12:7B:4F:C9:88:63:BB:79:80:0F:65:A9:55:00:18:55:50",
+        "0F:59:11:6B:E0:2A:89:51:FC:52:62:61:A9:EF:0A:81:CA:C6:E1:FE:3D:52:16:A8:D8:A3:DC:5D:A4:04:6B:33",
+        "97:4E:8F:3B:A8:20:51:8E:CC:0C:B2:D0:7B:11:86:11:2A:68:A1:59:65:C6:85:CC:72:8A:B6:55:B1:D7:C8:9F",
+        "9E:47:45:34:32:3A:C1:40:DC:0C:47:2E:F7:6A:5F:6A:1A:99:36:26:6E:9C:5F:A6:83:65:FB:CB:8F:A2:D1:4B",
+        "DA:C5:93:46:5F:37:EB:98:28:72:99:D5:3A:79:78:76:0E:7C:2A:21:FD:F5:E3:9E:D6:6F:B8:5A:59:E1:5B:FA",
+        "6D:6F:74:13:65:BB:3A:D6:6D:15:6C:7E:ED:D0:FE:59:F9:79:AD:F8:6E:D3:B4:F9:4A:90:10:90:79:B4:58:FB",
+        "9E:DA:B1:8F:22:2B:E7:E5:70:24:12:FB:DD:55:4F:DB:FC:B9:BE:3B:71:DB:BA:41:DA:DB:77:92:B2:27:A8:A6",
+        "13:3C:E8:7E:33:8B:17:90:99:A1:3E:D4:6A:C9:42:FF:7E:E5:A3:DF:B7:01:A0:AF:DC:17:B6:26:05:80:68:DA",
+        "71:28:B1:F5:CA:D3:55:21:6D:47:E1:0F:B4:90:27:C4:65:8E:B5:D9:CE:28:91:B8:B7:47:58:DA:1D:04:E0:48",
+        "63:E9:64:08:DF:A1:F1:43:0D:FA:2D:42:C2:A2:F3:92:70:A5:5B:32:31:CE:66:16:8D:A8:05:B1:D7:7A:DA:A2",
+        "35:54:3A:01:A4:A4:F2:09:CF:DE:79:8E:10:46:A1:8C:C1:23:9A:44:7D:20:88:5C:C3:15:F4:BF:FC:06:90:44")
