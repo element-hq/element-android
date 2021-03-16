e819ac5ad8341fa14d5af3a7f4eb6632138176da
Valere <valeref@matrix.org>
2021-03-16 11:00:39 +0100

    Removed unneeded id.home menu handling

--- vector/src/main/java/im/vector/app/features/spaces/SpaceCreationActivity.kt
+++ vector/src/main/java/im/vector/app/features/spaces/SpaceCreationActivity.kt
@@ -19,7 +19,6 @@ package im.vector.app.features.spaces
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.airbnb.mvrx.Loading
@@ -63,14 +62,6 @@ class SpaceCreationActivity : SimpleFragmentActivity(), CreateSpaceViewModel.Fac
                 CreateSpaceState.Step.AddRooms -> TODO()
             }
         }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            onBackPressed()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun initUiAndData() {
