54fbf7131cd5d7d18fd8ef849d9ff88a7b1c6ce9
Valere <valeref@matrix.org>
2021-03-25 11:13:14 +0100

    Invite Space/Room chooser

--- vector/src/main/java/im/vector/app/core/di/ScreenComponent.kt
+++ vector/src/main/java/im/vector/app/core/di/ScreenComponent.kt
@@ -77,6 +77,7 @@ import im.vector.app.features.settings.VectorSettingsActivity
 import im.vector.app.features.settings.devices.DeviceVerificationInfoBottomSheet
 import im.vector.app.features.share.IncomingShareActivity
 import im.vector.app.features.signout.soft.SoftLogoutActivity
+import im.vector.app.features.spaces.InviteRoomSpaceChooserBottomSheet
 import im.vector.app.features.spaces.ShareSpaceBottomSheet
 import im.vector.app.features.spaces.SpaceCreationActivity
 import im.vector.app.features.spaces.SpaceExploreActivity
@@ -181,6 +182,7 @@ interface ScreenComponent {
     fun inject(bottomSheet: MatrixToBottomSheet)
     fun inject(bottomSheet: ShareSpaceBottomSheet)
     fun inject(bottomSheet: SpaceSettingsMenuBottomSheet)
+    fun inject(bottomSheet: InviteRoomSpaceChooserBottomSheet)
 
     /* ==========================================================================================
      * Others
---
+++ vector/src/main/java/im/vector/app/features/spaces/InviteRoomSpaceChooserBottomSheet.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2021 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.spaces
+
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.airbnb.mvrx.args
+import im.vector.app.R
+import im.vector.app.core.di.ActiveSessionHolder
+import im.vector.app.core.di.ScreenComponent
+import im.vector.app.core.platform.VectorBaseBottomSheetDialogFragment
+import im.vector.app.databinding.BottomSheetSpaceInviteChooserBinding
+import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
+
+class InviteRoomSpaceChooserBottomSheet : VectorBaseBottomSheetDialogFragment<BottomSheetSpaceInviteChooserBinding>() {
+
+    @Parcelize
+    data class Args(
+            val spaceId: String,
+            val roomId: String
+    ) : Parcelable
+
+    override val showExpanded = true
+
+    private val inviteArgs: Args by args()
+
+    @Inject
+    lateinit var activeSessionHolder: ActiveSessionHolder
+
+    interface InteractionListener {
+        fun inviteToSpace(spaceId: String)
+        fun inviteToRoom(roomId: String)
+    }
+
+    var interactionListener: InteractionListener? = null
+
+    override fun injectWith(injector: ScreenComponent) {
+        injector.inject(this)
+    }
+
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): BottomSheetSpaceInviteChooserBinding {
+        return BottomSheetSpaceInviteChooserBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Not going for full view model for now, as it may change
+
+        val summary = activeSessionHolder.getSafeActiveSession()?.spaceService()?.getSpace(inviteArgs.spaceId)?.spaceSummary()
+
+        val spaceName = summary?.name
+
+        views.inviteToSpaceButton.isVisible = true
+        views.inviteToSpaceButton.title = getString(R.string.invite_to_space_with_name, spaceName)
+        views.inviteToSpaceButton.subTitle = getString(R.string.invite_to_space_with_name_desc, spaceName)
+        views.inviteToSpaceButton.debouncedClicks {
+            dismiss()
+            interactionListener?.inviteToSpace(inviteArgs.spaceId)
+        }
+
+        views.inviteToRoomOnly.isVisible = true
+        views.inviteToRoomOnly.title = getString(R.string.invite_just_to_this_room)
+        views.inviteToRoomOnly.subTitle = getString(R.string.invite_just_to_this_room_desc, spaceName)
+        views.inviteToRoomOnly.debouncedClicks {
+            dismiss()
+            interactionListener?.inviteToRoom(inviteArgs.roomId)
+        }
+    }
+
+    companion object {
+
+        fun newInstance(spaceId: String, roomId: String, interactionListener: InteractionListener)
+                : InviteRoomSpaceChooserBottomSheet {
+            return InviteRoomSpaceChooserBottomSheet().apply {
+                this.interactionListener = interactionListener
+                setArguments(Args(spaceId, roomId))
+            }
+        }
+    }
+}
