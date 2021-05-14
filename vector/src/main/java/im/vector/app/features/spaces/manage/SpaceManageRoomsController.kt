0d0b6a88100fb9d10fc878900332067e7eec2305
Valere <valeref@matrix.org>
2021-05-14 13:10:13 +0200

    Fix empty states for spaces

--- vector/src/main/java/im/vector/app/features/spaces/manage/SpaceManageRoomsController.kt
+++ vector/src/main/java/im/vector/app/features/spaces/manage/SpaceManageRoomsController.kt
@@ -19,9 +19,12 @@ package im.vector.app.features.spaces.manage
 import com.airbnb.epoxy.TypedEpoxyController
 import com.airbnb.mvrx.Fail
 import com.airbnb.mvrx.Incomplete
+import im.vector.app.R
 import im.vector.app.core.epoxy.errorWithRetryItem
 import im.vector.app.core.epoxy.loadingItem
 import im.vector.app.core.error.ErrorFormatter
+import im.vector.app.core.resources.StringProvider
+import im.vector.app.core.ui.list.genericFooterItem
 import im.vector.app.core.utils.DebouncedClickListener
 import im.vector.app.features.home.AvatarRenderer
 import org.matrix.android.sdk.api.session.room.model.RoomType
@@ -31,7 +34,8 @@ import javax.inject.Inject
 
 class SpaceManageRoomsController @Inject constructor(
         private val avatarRenderer: AvatarRenderer,
-        private val errorFormatter: ErrorFormatter
+        private val errorFormatter: ErrorFormatter,
+        private val stringProvider: StringProvider
 ) : TypedEpoxyController<SpaceManageRoomViewState>() {
 
     interface Listener {
@@ -67,17 +71,24 @@ class SpaceManageRoomsController @Inject constructor(
         matchFilter.filter = data.currentFilter
         val filteredResult = directChildren.filter { matchFilter.test(it) }
 
-        filteredResult.forEach { childInfo ->
-            roomManageSelectionItem {
-                id(childInfo.childRoomId)
-                matrixItem(childInfo.toMatrixItem())
-                avatarRenderer(host.avatarRenderer)
-                suggested(childInfo.suggested ?: false)
-                space(childInfo.roomType == RoomType.SPACE)
-                selected(data.selectedRooms.contains(childInfo.childRoomId))
-                itemClickListener(DebouncedClickListener({
-                    host.listener?.toggleSelection(childInfo)
-                }))
+        if (filteredResult.isEmpty()) {
+            genericFooterItem {
+                id("empty_result")
+                text(host.stringProvider.getString(R.string.no_result_placeholder))
+            }
+        } else {
+            filteredResult.forEach { childInfo ->
+                roomManageSelectionItem {
+                    id(childInfo.childRoomId)
+                    matrixItem(childInfo.toMatrixItem())
+                    avatarRenderer(host.avatarRenderer)
+                    suggested(childInfo.suggested ?: false)
+                    space(childInfo.roomType == RoomType.SPACE)
+                    selected(data.selectedRooms.contains(childInfo.childRoomId))
+                    itemClickListener(DebouncedClickListener({
+                        host.listener?.toggleSelection(childInfo)
+                    }))
+                }
             }
         }
     }
