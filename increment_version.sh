e76245b2787d3bfe3da4e920c051eaf1399ddd6c
SpiritCroc <dev@spiritcroc.de>
2021-06-04 11:28:55 +0200

    increment_version.sh: Exclude translation updates
    Change-Id: I6a47969a242ec3dbff3e61ace1dcd51ea2a493df

--- increment_version.sh
+++ increment_version.sh
@@ -140,7 +140,10 @@ git_changelog() {
         | grep -v "Automatic SchildiChat string correction" \
         | grep -v 'merge_helpers\|README\|increment_version' \
         | grep -v "\\.sh" \
-        | grep -v "Update string correction"
+        | grep -v "Update string correction" \
+        | grep -v "Added translation using Weblate" \
+        | grep -v "Translated using Weblate" \
+        | grep -v "weblate/sc"
 }
 
 changelog_dir=fastlane/metadata/android/en-US/changelogs
