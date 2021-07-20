ae4a6af006b5b0b23b787bce0e929cd0164cac57
yostyle <yoanp@element.io>
2021-07-20 17:08:53 +0200

    open DM if a user invite by email already exist

--- vector/src/main/java/im/vector/app/features/home/HomeDetailViewModel.kt
+++ vector/src/main/java/im/vector/app/features/home/HomeDetailViewModel.kt
@@ -186,13 +186,14 @@ class HomeDetailViewModel @AssistedInject constructor(@Assisted initialState: Ho
             if (data.isNullOrEmpty()) {
                 _viewEvents.post(HomeDetailViewEvents.GetPlatform(action.email))
             } else {
-                _viewEvents.post(HomeDetailViewEvents.InviteIgnoredForDiscoveredUser(action.email))
+                val userId = data.find { it.threePid.value == action.email }?.matrixId
+                userId?.let { _viewEvents.post(HomeDetailViewEvents.InviteIgnoredForDiscoveredUser(it)) }
             }
         }
     }
 
     private fun handleSelectContact(action: HomeDetailAction.SelectContact) {
-        val directRoomId = session.getExistingDirectRoomWithUser(action.user.userId)
+        val directRoomId = session.getExistingDirectRoomWithUser(action.userId)
         if (directRoomId != null) {
             _viewEvents.post(HomeDetailViewEvents.OpenDirectChat(directRoomId))
         } else {
