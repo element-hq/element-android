8d0c133ddeb365e185f3d6b230f78681addaa58f
SpiritCroc <dev@spiritcroc.de>
2021-07-21 11:20:26 +0200

    Theme code backgrounds in markdown
    Change-Id: Ia37dc51961e990e5b7a2c9c6890da01bcf076dd6

--- vector/src/main/java/im/vector/app/features/html/EventHtmlRenderer.kt
+++ vector/src/main/java/im/vector/app/features/html/EventHtmlRenderer.kt
@@ -19,8 +19,12 @@ package im.vector.app.features.html
 import android.content.Context
 import android.text.Spannable
 import androidx.core.text.toSpannable
+import im.vector.app.R
 import im.vector.app.core.resources.ColorProvider
+import im.vector.app.features.themes.ThemeUtils
+import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
+import io.noties.markwon.core.MarkwonTheme
 import io.noties.markwon.html.HtmlPlugin
 import org.commonmark.node.Node
 import timber.log.Timber
@@ -28,16 +32,44 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class EventHtmlRenderer @Inject constructor(htmlConfigure: MatrixHtmlPluginConfigure,
-                                            context: Context) {
+class EventHtmlRenderer @Inject constructor(private val htmlConfigure: MatrixHtmlPluginConfigure,
+                                            private val context: Context) {
+
+    private fun resolveCodeBlockBackground() =
+        ThemeUtils.getColor(context, R.attr.code_block_bg_color)
+
+    private var codeBlockBackground: Int = resolveCodeBlockBackground()
 
     interface PostProcessor {
         fun afterRender(renderedText: Spannable)
     }
 
-    private val markwon = Markwon.builder(context)
-            .usePlugin(HtmlPlugin.create(htmlConfigure))
+    private fun buildMarkwon() = Markwon.builder(context)
+            .usePlugins(listOf(
+                    HtmlPlugin.create(htmlConfigure),
+                    object: AbstractMarkwonPlugin() {
+                        override fun configureTheme(builder: MarkwonTheme.Builder) {
+                            super.configureTheme(builder)
+                            builder.codeBlockBackgroundColor(codeBlockBackground)
+                                    .codeBackgroundColor(codeBlockBackground)
+                        }
+                    }
+            ))
             .build()
+
+    private var markwon: Markwon = buildMarkwon()
+    get() {
+        val newCodeBlockBackground = resolveCodeBlockBackground()
+        if (codeBlockBackground != newCodeBlockBackground) {
+            codeBlockBackground = newCodeBlockBackground
+            field = buildMarkwon()
+        }
+        return field
+    }
+
+    fun invalidateColors() {
+        markwon = buildMarkwon()
+    }
 
     fun parse(text: String): Node {
         return markwon.parse(text)
