f64366e3ff0ccee59c7dd6d82a633d76c4afa184
p_0g_8mm3_ <37022952+pr0gr8mm3r@users.noreply.github.com>
2021-08-05 00:00:12 +0200

    Fix attachment menu animation order

--- vector/src/main/java/im/vector/app/features/attachments/AttachmentTypeSelectorView.kt
+++ vector/src/main/java/im/vector/app/features/attachments/AttachmentTypeSelectorView.kt
@@ -102,12 +102,12 @@ class AttachmentTypeSelectorView(context: Context,
         contentView.doOnNextLayout {
             animateWindowInCircular(anchor, contentView)
         }
-        animateButtonIn(views.attachmentGalleryButton, ANIMATION_DURATION / 2)
+        animateButtonIn(views.attachmentGalleryButton, ANIMATION_DURATION / 4)
         animateButtonIn(views.attachmentCameraButton, ANIMATION_DURATION / 2)
-        animateButtonIn(views.attachmentFileButton, ANIMATION_DURATION / 4)
-        animateButtonIn(views.attachmentAudioButton, ANIMATION_DURATION / 2)
+        animateButtonIn(views.attachmentFileButton, ANIMATION_DURATION / 2)
+        animateButtonIn(views.attachmentAudioButton, 0)
         animateButtonIn(views.attachmentContactButton, ANIMATION_DURATION / 4)
-        animateButtonIn(views.attachmentStickersButton, 0)
+        animateButtonIn(views.attachmentStickersButton, ANIMATION_DURATION / 2)
     }
 
     override fun dismiss() {
