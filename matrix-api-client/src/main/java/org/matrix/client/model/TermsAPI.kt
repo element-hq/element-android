b9efc653275447e6feb76cdfe078da842f584166
ganfra <francoisg@element.io>
2021-08-19 10:36:19 +0200

    Extract matrix client: TermsAPI

---
+++ matrix-api-client/src/main/java/org/matrix/client/model/TermsAPI.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 The Matrix.org Foundation C.I.C.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.matrix.client.model
+
+import org.matrix.client.utils.HttpHeaders
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Url
+
+internal interface TermsAPI {
+    /**
+     * This request does not require authentication
+     */
+    @GET
+    suspend fun getTerms(@Url url: String): TermsResponse
+
+    /**
+     * This request requires authentication
+     */
+    @POST
+    suspend fun agreeToTerms(@Url url: String,
+                             @Body params: AcceptTermsBody,
+                             @Header(HttpHeaders.Authorization) token: String)
+}
