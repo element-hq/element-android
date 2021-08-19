10f8e57062bd2ce19ee5a0655620cdf195a17fec
ganfra <francoisg@element.io>
2021-08-19 10:34:45 +0200

    Extract matrix client: SyncAPI

---
+++ matrix-api-client/src/main/java/org/matrix/client/api/SyncAPI.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 The Matrix.org Foundation C.I.C.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.matrix.client.api
+
+import okhttp3.ResponseBody
+import org.matrix.client.model.SyncResponse
+import org.matrix.client.utils.NetworkConstants
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+import retrofit2.http.Streaming
+
+internal interface SyncAPI {
+    /**
+     * Set all the timeouts to 1 minute by default
+     */
+    @GET(NetworkConstants.URI_API_PREFIX_PATH_R0 + "sync")
+    suspend fun sync(@QueryMap params: Map<String, String>): SyncResponse
+
+    /**
+     * Set all the timeouts to 1 minute by default
+     */
+    @Streaming
+    @GET(NetworkConstants.URI_API_PREFIX_PATH_R0 + "sync")
+    fun syncStream(@QueryMap params: Map<String, String>): Call<ResponseBody>
+}
