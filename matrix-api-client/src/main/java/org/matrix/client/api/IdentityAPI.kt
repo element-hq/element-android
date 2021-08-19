46bb24e8cad6e1afda4a5f53526396fdc362130b
ganfra <francoisg@element.io>
2021-08-19 09:30:06 +0200

    Extract matrix client: IdentityAPI

---
+++ matrix-api-client/src/main/java/org/matrix/client/api/IdentityAPI.kt
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020 The Matrix.org Foundation C.I.C.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.matrix.client.api
+
+import org.matrix.client.model.IdentityAccountResponse
+import org.matrix.client.model.IdentityHashDetailResponse
+import org.matrix.client.model.IdentityLookUpParams
+import org.matrix.client.model.IdentityLookUpResponse
+import org.matrix.client.model.IdentityRequestOwnershipParams
+import org.matrix.client.model.IdentityRequestTokenForEmailBody
+import org.matrix.client.model.IdentityRequestTokenForMsisdnBody
+import org.matrix.client.model.IdentityRequestTokenResponse
+import org.matrix.client.model.SuccessResult
+import org.matrix.client.utils.NetworkConstants
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+/**
+ * Ref: https://matrix.org/docs/spec/identity_service/latest
+ * This contain the requests which need an identity server token
+ */
+internal interface IdentityAPI {
+    /**
+     * Gets information about what user owns the access token used in the request.
+     * Will return a 403 for when terms are not signed
+     * Ref: https://matrix.org/docs/spec/identity_service/latest#get-matrix-identity-v2-account
+     */
+    @GET(NetworkConstants.URI_IDENTITY_PATH_V2 + "account")
+    suspend fun getAccount(): IdentityAccountResponse
+
+    /**
+     * Logs out the access token, preventing it from being used to authenticate future requests to the server.
+     */
+    @POST(NetworkConstants.URI_IDENTITY_PATH_V2 + "account/logout")
+    suspend fun logout()
+
+    /**
+     * Request the hash detail to request a bunch of 3PIDs
+     * Ref: https://matrix.org/docs/spec/identity_service/latest#get-matrix-identity-v2-hash-details
+     */
+    @GET(NetworkConstants.URI_IDENTITY_PATH_V2 + "hash_details")
+    suspend fun hashDetails(): IdentityHashDetailResponse
+
+    /**
+     * Request a bunch of 3PIDs
+     * Ref: https://matrix.org/docs/spec/identity_service/latest#post-matrix-identity-v2-lookup
+     *
+     * @param body the body request
+     */
+    @POST(NetworkConstants.URI_IDENTITY_PATH_V2 + "lookup")
+    suspend fun lookup(@Body body: IdentityLookUpParams): IdentityLookUpResponse
+
+    /**
+     * Create a session to change the bind status of an email to an identity server
+     * The identity server will also send an email
+     *
+     * @param body
+     * @return the sid
+     */
+    @POST(NetworkConstants.URI_IDENTITY_PATH_V2 + "validate/email/requestToken")
+    suspend fun requestTokenToBindEmail(@Body body: IdentityRequestTokenForEmailBody): IdentityRequestTokenResponse
+
+    /**
+     * Create a session to change the bind status of an phone number to an identity server
+     * The identity server will also send an SMS on the ThreePid provided
+     *
+     * @param body
+     * @return the sid
+     */
+    @POST(NetworkConstants.URI_IDENTITY_PATH_V2 + "validate/msisdn/requestToken")
+    suspend fun requestTokenToBindMsisdn(@Body body: IdentityRequestTokenForMsisdnBody): IdentityRequestTokenResponse
+
+    /**
+     * Validate ownership of an email address, or a phone number.
+     * Ref:
+     * - https://matrix.org/docs/spec/identity_service/latest#post-matrix-identity-v2-validate-msisdn-submittoken
+     * - https://matrix.org/docs/spec/identity_service/latest#post-matrix-identity-v2-validate-email-submittoken
+     */
+    @POST(NetworkConstants.URI_IDENTITY_PATH_V2 + "validate/{medium}/submitToken")
+    suspend fun submitToken(@Path("medium") medium: String,
+                            @Body body: IdentityRequestOwnershipParams): SuccessResult
+}
