aff787bb29cc887a03db81412e51a613a5f53e59
Adam Brown <adampsbrown@gmail.com>
2021-09-28 08:52:55 +0100

    extracting the test fakes to their own package

--- matrix-sdk-android/src/test/java/org/matrix/android/sdk/internal/session/pushers/DefaultAddPusherTaskTest.kt
+++ matrix-sdk-android/src/test/java/org/matrix/android/sdk/internal/session/pushers/DefaultAddPusherTaskTest.kt
@@ -16,26 +16,16 @@
 
 package org.matrix.android.sdk.internal.session.pushers
 
-import com.zhuinden.monarchy.Monarchy
-import io.mockk.MockKVerificationScope
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.verify
-import io.realm.Realm
-import io.realm.RealmModel
-import io.realm.RealmQuery
-import io.realm.kotlin.where
 import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.internal.assertFailsWith
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
-import org.matrix.android.sdk.api.failure.GlobalError
 import org.matrix.android.sdk.api.session.pushers.PusherState
 import org.matrix.android.sdk.internal.database.model.PusherEntity
-import org.matrix.android.sdk.internal.network.GlobalErrorReceiver
-import org.matrix.android.sdk.internal.util.awaitTransaction
+import org.matrix.android.sdk.test.fakes.FakeGlobalErrorReceiver
+import org.matrix.android.sdk.test.fakes.FakeMonarchy
+import org.matrix.android.sdk.test.fakes.FakePushersAPI
+import org.matrix.android.sdk.test.fakes.FakeRequestExecutor
 import java.net.SocketException
 
 private val A_JSON_PUSHER = JsonPusher(
@@ -109,70 +99,5 @@ class DefaultAddPusherTaskTest {
         assertFailsWith<SocketException> {
             runBlocking { addPusherTask.execute(AddPusherTask.Params(A_JSON_PUSHER)) }
         }
-    }
-}
-
-internal class FakePushersAPI : PushersAPI {
-
-    private var setRequestPayload: JsonPusher? = null
-    private var error: Throwable? = null
-
-    override suspend fun getPushers(): GetPushersResponse {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun setPusher(jsonPusher: JsonPusher) {
-        error?.let { throw it }
-        setRequestPayload = jsonPusher
-    }
-
-    fun verifySetPusher(payload: JsonPusher) {
-        this.setRequestPayload shouldBeEqualTo payload
-    }
-
-    fun givenSetPusherErrors(error: Throwable) {
-        this.error = error
-    }
-}
-
-internal class FakeMonarchy {
-
-    val instance = mockk<Monarchy>()
-    private val realm = mockk<Realm>(relaxed = true)
-
-    init {
-        mockkStatic("org.matrix.android.sdk.internal.util.MonarchyKt")
-        coEvery {
-            instance.awaitTransaction(any<suspend (Realm) -> Any>())
-        } coAnswers {
-            secondArg<suspend (Realm) -> Any>().invoke(realm)
-        }
-    }
-
-    inline fun <reified T : RealmModel> givenWhereReturns(result: T?) {
-        val queryResult = mockk<RealmQuery<T>>(relaxed = true)
-        every { queryResult.findFirst() } returns result
-        every { realm.where<T>() } returns queryResult
-    }
-
-    inline fun <reified T : RealmModel> verifyInsertOrUpdate(crossinline verification: MockKVerificationScope.() -> T) {
-        verify { realm.insertOrUpdate(verification()) }
-    }
-}
-
-internal class FakeRequestExecutor : RequestExecutor {
-
-    override suspend fun <DATA> executeRequest(globalErrorReceiver: GlobalErrorReceiver?,
-                                               canRetry: Boolean,
-                                               maxDelayBeforeRetry: Long,
-                                               maxRetriesCount: Int,
-                                               requestBlock: suspend () -> DATA): DATA {
-        return requestBlock()
-    }
-}
-
-internal class FakeGlobalErrorReceiver : GlobalErrorReceiver {
-    override fun handleGlobalError(globalError: GlobalError) {
-        // do nothing
     }
 }
