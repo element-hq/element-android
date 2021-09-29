36b761748382f1183cc2fc49a2e1659185c14e33
Benoit Marty <benoit@matrix.org>
2021-09-29 16:57:10 +0200

    Add comment about copy/paste code

--- vector/src/main/java/im/vector/app/features/displayname/Extension.kt
+++ vector/src/main/java/im/vector/app/features/displayname/Extension.kt
@@ -19,6 +19,7 @@ package im.vector.app.features.displayname
 import org.matrix.android.sdk.api.util.MatrixItem
 
 fun MatrixItem.getBestName(): String {
+    // Note: this code is copied from [DisplayNameResolver] in the SDK
     return if (this is MatrixItem.GroupItem || this is MatrixItem.RoomAliasItem) {
         // Best name is the id, and we keep the displayName of the room for the case we need the first letter
         id
