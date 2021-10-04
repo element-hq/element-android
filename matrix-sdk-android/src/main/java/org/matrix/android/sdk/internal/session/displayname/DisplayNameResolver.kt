0a6b71d27db34953af229af0d41502d57b601603
Benoit Marty <benoit@matrix.org>
2021-10-04 09:58:14 +0200

    ktlint

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/displayname/DisplayNameResolver.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/displayname/DisplayNameResolver.kt
@@ -35,4 +35,3 @@ internal class DisplayNameResolver @Inject constructor(
         }
     }
 }
-
--- vector/src/main/java/im/vector/app/features/displayname/VectorMatrixItemDisplayNameFallbackProvider.kt
+++ vector/src/main/java/im/vector/app/features/displayname/VectorMatrixItemDisplayNameFallbackProvider.kt
@@ -20,7 +20,7 @@ import org.matrix.android.sdk.api.MatrixItemDisplayNameFallbackProvider
 import org.matrix.android.sdk.api.util.MatrixItem
 
 // Used to provide the fallback to the MatrixSDK, in the MatrixConfiguration
-object VectorMatrixItemDisplayNameFallbackProvider: MatrixItemDisplayNameFallbackProvider {
+object VectorMatrixItemDisplayNameFallbackProvider : MatrixItemDisplayNameFallbackProvider {
     override fun getDefaultName(matrixItem: MatrixItem): String {
         // Can customize something from the id if necessary here
         return matrixItem.id
