0f4ec65b7a501f4f0138f737fbee99f6896f0d32
Adam Brown <adampsbrown@gmail.com>
2021-10-06 19:03:22 +0100

    creating the notifications separate to where they're displayed - also handles when the event diff means the notifications should be removed

---
+++ vector/src/main/java/im/vector/app/features/notifications/NotificationFactory.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2021 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.notifications
+
+import android.app.Notification
+import javax.inject.Inject
+
+class NotificationFactory @Inject constructor(
+        private val notificationUtils: NotificationUtils,
+        private val roomGroupMessageCreator: RoomGroupMessageCreator,
+        private val summaryGroupMessageCreator: SummaryGroupMessageCreator
+) {
+
+    fun Map<String, List<NotifiableMessageEvent>>.toNotifications(myUserDisplayName: String, myUserAvatarUrl: String?): List<RoomNotification> {
+        return this.map { (roomId, events) ->
+            when {
+                events.hasNoEventsToDisplay() -> RoomNotification.EmptyRoom(roomId)
+                else                          -> roomGroupMessageCreator.createRoomMessage(events, roomId, myUserDisplayName, myUserAvatarUrl)
+            }
+        }
+    }
+
+    private fun List<NotifiableMessageEvent>.hasNoEventsToDisplay() = isEmpty() || all { it.canNotBeDisplayed() }
+
+    private fun NotifiableMessageEvent.canNotBeDisplayed() = hasBeenDisplayed || isRedacted
+
+    fun Map<String, InviteNotifiableEvent?>.toNotifications(myUserId: String): List<OneShotNotification> {
+        return this.map { (roomId, event) ->
+            when (event) {
+                null -> OneShotNotification.Removed(key = roomId)
+                else -> OneShotNotification.Append(
+                        notificationUtils.buildRoomInvitationNotification(event, myUserId),
+                        OneShotNotification.Append.Meta(key = roomId, summaryLine = event.description, isNoisy = event.noisy)
+                )
+            }
+        }
+    }
+
+    @JvmName("toNotificationsSimpleNotifiableEvent")
+    fun Map<String, SimpleNotifiableEvent?>.toNotifications(myUserId: String): List<OneShotNotification> {
+        return this.map { (eventId, event) ->
+            when (event) {
+                null -> OneShotNotification.Removed(key = eventId)
+                else -> OneShotNotification.Append(
+                        notificationUtils.buildSimpleEventNotification(event, myUserId),
+                        OneShotNotification.Append.Meta(key = eventId, summaryLine = event.description, isNoisy = event.noisy)
+                )
+            }
+        }
+    }
+
+    fun createSummaryNotification(roomNotifications: List<RoomNotification>,
+                                  invitationNotifications: List<OneShotNotification>,
+                                  simpleNotifications: List<OneShotNotification>,
+                                  useCompleteNotificationFormat: Boolean): Notification {
+        return summaryGroupMessageCreator.createSummaryNotification(
+                roomNotifications = roomNotifications.mapToMeta(),
+                invitationNotifications = invitationNotifications.mapToMeta(),
+                simpleNotifications = simpleNotifications.mapToMeta(),
+                useCompleteNotificationFormat = useCompleteNotificationFormat
+        )
+    }
+}
+
+private fun List<RoomNotification>.mapToMeta() = filterIsInstance<RoomNotification.Message>().map { it.meta }
+
+@JvmName("mapToMetaOneShotNotification")
+private fun List<OneShotNotification>.mapToMeta() = filterIsInstance<OneShotNotification.Append>().map { it.meta }
+
+sealed interface RoomNotification {
+    data class EmptyRoom(val roomId: String) : RoomNotification
+    data class Message(val notification: Notification, val meta: Meta) : RoomNotification {
+        data class Meta(
+                val summaryLine: CharSequence,
+                val messageCount: Int,
+                val latestTimestamp: Long,
+                val roomId: String,
+                val shouldBing: Boolean
+        )
+    }
+}
+
+sealed interface OneShotNotification {
+    data class Removed(val key: String) : OneShotNotification
+    data class Append(val notification: Notification, val meta: Meta) : OneShotNotification {
+        data class Meta(
+                val key: String,
+                val summaryLine: CharSequence,
+                val isNoisy: Boolean
+        )
+    }
+}
---
+++ vector/src/test/java/im/vector/app/features/notifications/NotificationFactoryTest.kt
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2021 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.notifications
+
+import im.vector.app.test.fakes.FakeNotificationUtils
+import im.vector.app.test.fakes.FakeRoomGroupMessageCreator
+import im.vector.app.test.fakes.FakeSummaryGroupMessageCreator
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Test
+
+private const val MY_USER_ID = "user-id"
+private const val A_ROOM_ID = "room-id"
+private const val AN_EVENT_ID = "event-id"
+
+private val MY_AVATAR_URL: String? = null
+private val AN_INVITATION_EVENT = anInviteNotifiableEvent(roomId = A_ROOM_ID)
+private val A_SIMPLE_EVENT = aSimpleNotifiableEvent(eventId = AN_EVENT_ID)
+private val A_MESSAGE_EVENT = aNotifiableMessageEvent(eventId = AN_EVENT_ID, roomId = A_ROOM_ID)
+
+class NotificationFactoryTest {
+
+    private val notificationUtils = FakeNotificationUtils()
+    private val roomGroupMessageCreator = FakeRoomGroupMessageCreator()
+    private val summaryGroupMessageCreator = FakeSummaryGroupMessageCreator()
+
+    private val notificationFactory = NotificationFactory(
+            notificationUtils.instance,
+            roomGroupMessageCreator.instance,
+            summaryGroupMessageCreator.instance
+    )
+
+    @Test
+    fun `given a room invitation when mapping to notification then is Append`() = testWith(notificationFactory) {
+        val expectedNotification = notificationUtils.givenBuildRoomInvitationNotificationFor(AN_INVITATION_EVENT, MY_USER_ID)
+        val roomInvitation = mapOf(A_ROOM_ID to AN_INVITATION_EVENT)
+
+        val result = roomInvitation.toNotifications(MY_USER_ID)
+
+        result shouldBeEqualTo listOf(OneShotNotification.Append(
+                notification = expectedNotification,
+                meta = OneShotNotification.Append.Meta(
+                        key = A_ROOM_ID,
+                        summaryLine = AN_INVITATION_EVENT.description,
+                        isNoisy = AN_INVITATION_EVENT.noisy
+                ))
+        )
+    }
+
+    @Test
+    fun `given a missing event in room invitation when mapping to notification then is Removed`() = testWith(notificationFactory) {
+        val missingEventRoomInvitation: Map<String, InviteNotifiableEvent?> = mapOf(A_ROOM_ID to null)
+
+        val result = missingEventRoomInvitation.toNotifications(MY_USER_ID)
+
+        result shouldBeEqualTo listOf(OneShotNotification.Removed(
+                key = A_ROOM_ID
+        ))
+    }
+
+    @Test
+    fun `given a simple event when mapping to notification then is Append`() = testWith(notificationFactory) {
+        val expectedNotification = notificationUtils.givenBuildSimpleInvitationNotificationFor(A_SIMPLE_EVENT, MY_USER_ID)
+        val roomInvitation = mapOf(AN_EVENT_ID to A_SIMPLE_EVENT)
+
+        val result = roomInvitation.toNotifications(MY_USER_ID)
+
+        result shouldBeEqualTo listOf(OneShotNotification.Append(
+                notification = expectedNotification,
+                meta = OneShotNotification.Append.Meta(
+                        key = AN_EVENT_ID,
+                        summaryLine = A_SIMPLE_EVENT.description,
+                        isNoisy = A_SIMPLE_EVENT.noisy
+                ))
+        )
+    }
+
+    @Test
+    fun `given a missing simple event when mapping to notification then is Removed`() = testWith(notificationFactory) {
+        val missingEventRoomInvitation: Map<String, SimpleNotifiableEvent?> = mapOf(AN_EVENT_ID to null)
+
+        val result = missingEventRoomInvitation.toNotifications(MY_USER_ID)
+
+        result shouldBeEqualTo listOf(OneShotNotification.Removed(
+                key = AN_EVENT_ID
+        ))
+    }
+
+    @Test
+    fun `given room with message when mapping to notification then delegates to room group message creator`() = testWith(notificationFactory) {
+        val events = listOf(A_MESSAGE_EVENT)
+        val expectedNotification = roomGroupMessageCreator.givenCreatesRoomMessageFor(events, A_ROOM_ID, MY_USER_ID, MY_AVATAR_URL)
+        val roomWithMessage = mapOf(A_ROOM_ID to events)
+
+        val result = roomWithMessage.toNotifications(MY_USER_ID, MY_AVATAR_URL)
+
+        result shouldBeEqualTo listOf(expectedNotification)
+    }
+
+    @Test
+    fun `given a room with no events to display when mapping to notification then is Empty`() = testWith(notificationFactory) {
+        val emptyRoom: Map<String, List<NotifiableMessageEvent>> = mapOf(A_ROOM_ID to emptyList())
+
+        val result = emptyRoom.toNotifications(MY_USER_ID, MY_AVATAR_URL)
+
+        result shouldBeEqualTo listOf(RoomNotification.EmptyRoom(
+                roomId = A_ROOM_ID
+        ))
+    }
+
+    @Test
+    fun `given a room with only redacted events when mapping to notification then is Empty`() = testWith(notificationFactory) {
+        val redactedRoom = mapOf(A_ROOM_ID to listOf(A_MESSAGE_EVENT.copy().apply { isRedacted = true }))
+
+        val result = redactedRoom.toNotifications(MY_USER_ID, MY_AVATAR_URL)
+
+        result shouldBeEqualTo listOf(RoomNotification.EmptyRoom(
+                roomId = A_ROOM_ID
+        ))
+    }
+}
+
+fun <T> testWith(receiver: T, block: T.() -> Unit) {
+    receiver.block()
+}
