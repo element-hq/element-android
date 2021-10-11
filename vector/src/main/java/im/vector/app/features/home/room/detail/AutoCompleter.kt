506dfe5fea8dbe6d2e84b365388c7a7e26d43b77
ariskotsomitopoulos <aris.kotsomitopoulos@gmail.com>
2021-10-11 11:46:37 +0300

    Adding trailing space " " or ": " if the user started a sentence by mentioning someone,

--- vector/src/main/java/im/vector/app/features/home/room/detail/AutoCompleter.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/AutoCompleter.kt
@@ -220,8 +220,16 @@ class AutoCompleter @AssistedInject constructor(
         // Replace the word by its completion
         val displayName = matrixItem.getBestName()
 
-        // with a trailing space
-        editable.replace(startIndex, endIndex, "$displayName ")
+
+        // Adding trailing space " " or ": " if the user started mention someone
+        val displayNameSuffix =
+                if (firstChar == "@" && editText.length() == 1) {
+                    ": "
+                } else {
+                    " "
+                }
+
+        editable.replace(startIndex, endIndex, "$displayName$displayNameSuffix")
 
         // Add the span
         val span = PillImageSpan(
