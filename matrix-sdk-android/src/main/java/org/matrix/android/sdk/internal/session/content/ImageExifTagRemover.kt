461a47cfb07875de5923087c7ad995a60c9e7390
ariskotsomitopoulos <aris.kotsomitopoulos@gmail.com>
2021-10-18 23:56:00 +0300

    Refactor removeSensitiveJpegExifTags

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/content/ImageExifTagRemover.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/content/ImageExifTagRemover.kt
@@ -23,6 +23,7 @@ import org.apache.sanselan.formats.jpeg.exifRewrite.ExifRewriter
 import org.apache.sanselan.formats.tiff.constants.ExifTagConstants
 import org.apache.sanselan.formats.tiff.constants.GPSTagConstants
 import org.matrix.android.sdk.api.MatrixCoroutineDispatchers
+import org.matrix.android.sdk.api.extensions.tryOrNull
 import org.matrix.android.sdk.internal.util.TemporaryFileCreator
 import java.io.BufferedOutputStream
 import java.io.File
@@ -45,37 +46,36 @@ internal class ImageExifTagRemover @Inject constructor(
      * @return the new scrubbed image file, or the original file if the operation failed
      */
     suspend fun removeSensitiveJpegExifTags(jpegImageFile: File): File = withContext(coroutineDispatchers.io) {
+        val outputSet = tryOrNull("Unable to read JpegImageMetadata") {
+            (Sanselan.getMetadata(jpegImageFile) as? JpegImageMetadata)?.exif?.outputSet
+        } ?: return@withContext jpegImageFile
+
+        tryOrNull("Unable to remove ExifData") {
+            outputSet.removeField(ExifTagConstants.EXIF_TAG_GPSINFO)
+            outputSet.removeField(ExifTagConstants.EXIF_TAG_SUBJECT_LOCATION_1)
+            outputSet.removeField(ExifTagConstants.EXIF_TAG_SUBJECT_LOCATION_2)
+            outputSet.removeField(ExifTagConstants.EXIF_TAG_USER_COMMENT)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_ALTITUDE)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_ALTITUDE_REF)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_LONGITUDE)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_LONGITUDE_REF)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LONGITUDE)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LONGITUDE_REF)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_LATITUDE)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_LATITUDE_REF)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LATITUDE)
+            outputSet.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LATITUDE_REF)
+        } ?: return@withContext jpegImageFile
+
         val scrubbedFile = temporaryFileCreator.create()
-        runCatching {
+        return@withContext runCatching {
             FileOutputStream(scrubbedFile).use { fos ->
                 val outputStream = BufferedOutputStream(fos)
-                val outputSet = (Sanselan.getMetadata(jpegImageFile) as? JpegImageMetadata)?.exif?.outputSet
-
-                outputSet?.let {
-                    it.removeField(ExifTagConstants.EXIF_TAG_GPSINFO)
-                    it.removeField(ExifTagConstants.EXIF_TAG_SUBJECT_LOCATION_1)
-                    it.removeField(ExifTagConstants.EXIF_TAG_SUBJECT_LOCATION_2)
-                    it.removeField(ExifTagConstants.EXIF_TAG_USER_COMMENT)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_ALTITUDE)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_ALTITUDE_REF)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_LONGITUDE)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_LONGITUDE_REF)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LONGITUDE)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LONGITUDE_REF)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_LATITUDE)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_LATITUDE_REF)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LATITUDE)
-                    it.removeField(GPSTagConstants.GPS_TAG_GPS_DEST_LATITUDE_REF)
-                    ExifRewriter().updateExifMetadataLossless(jpegImageFile, outputStream, it)
-                } ?: run {
-                    scrubbedFile.delete()
-                    return@withContext jpegImageFile
-                }
+                ExifRewriter().updateExifMetadataLossless(jpegImageFile, outputStream, outputSet)
             }
-            scrubbedFile
         }.fold(
                 onSuccess = {
-                    it
+                    scrubbedFile
                 },
                 onFailure = {
                     scrubbedFile.delete()
