23adfb9c078b22932bb6779937c14f9571ba5e76
Florian Renaud <florianr@element.io>
2021-10-25 15:20:17 +0200

    Update terms and conditions URL

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
@@ -19,7 +19,7 @@ package im.vector.app.features.settings
 object VectorSettingsUrls {
 
     const val COPYRIGHT = "https://element.io/copyright"
-    const val TAC = "https://element.io/terms-of-service"
+    const val TAC = "https://www.tchap.gouv.fr/tac.html"
     const val PRIVACY_POLICY = "https://element.io/privacy"
     const val DISCLAIMER_URL = "https://element.io/previously-riot"
     const val THIRD_PARTY_LICENSES = "file:///android_asset/open_source_licenses.html"
