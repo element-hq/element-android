657a61337481481ae5dd57f536d9e718d9729f9a
Adam Brown <adampsbrown@gmail.com>
2021-11-05 15:47:21 +0000

    ensuring we wait for the create room fragment to be ready

--- vector/src/androidTest/java/im/vector/app/ui/robot/NewRoomRobot.kt
+++ vector/src/androidTest/java/im/vector/app/ui/robot/NewRoomRobot.kt
@@ -16,21 +16,24 @@
 
 package im.vector.app.ui.robot
 
-import androidx.test.espresso.Espresso
-import com.adevinta.android.barista.interaction.BaristaClickInteractions
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
 import im.vector.app.R
+import im.vector.app.espresso.tools.waitUntilViewVisible
 
 class NewRoomRobot(
         var createdRoom: Boolean = false
 ) {
 
     fun createNewRoom(block: CreateNewRoomRobot.() -> Unit) {
-        BaristaClickInteractions.clickOn(R.string.create_new_room)
+        clickOn(R.string.create_new_room)
+        waitUntilViewVisible(withId(R.id.createRoomForm))
         val createNewRoomRobot = CreateNewRoomRobot()
         block(createNewRoomRobot)
         createdRoom = createNewRoomRobot.createdRoom
         if (!createNewRoomRobot.createdRoom) {
-            Espresso.pressBack()
+            pressBack()
         }
     }
 }
