e8fb4085795b0b4405348ad93a16de8e8e8c1817
Benoit Marty <benoit@matrix.org>
2021-11-05 12:48:27 +0100

    Limit the number of pushed shortcuts

--- vector/src/main/java/im/vector/app/features/home/ShortcutsHandler.kt
+++ vector/src/main/java/im/vector/app/features/home/ShortcutsHandler.kt
@@ -50,6 +50,7 @@ class ShortcutsHandler @Inject constructor(
 ) : PinCodeStoreListener {
 
     private val isRequestPinShortcutSupported = ShortcutManagerCompat.isRequestPinShortcutSupported(context)
+    private val maxShortcutCountPerActivity = ShortcutManagerCompat.getMaxShortcutCountPerActivity(context)
 
     // Value will be set correctly if necessary
     private var hasPinCode = AtomicBoolean(true)
@@ -101,9 +102,11 @@ class ShortcutsHandler @Inject constructor(
             // No shortcut in this case (privacy)
             ShortcutManagerCompat.removeAllDynamicShortcuts(context)
         } else {
-            val shortcuts = rooms.mapIndexed { index, room ->
-                shortcutCreator.create(room, index)
-            }
+            val shortcuts = rooms
+                    .take(maxShortcutCountPerActivity)
+                    .mapIndexed { index, room ->
+                        shortcutCreator.create(room, index)
+                    }
 
             shortcuts.forEach { shortcut ->
                 ShortcutManagerCompat.pushDynamicShortcut(context, shortcut)
