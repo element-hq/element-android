153694ca8716f7b575e6df2859631a7cc98f56c8
Benoit Marty <benoit@matrix.org>
2021-11-16 17:45:33 +0100

    Small cleanup

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/util/ContentUtils.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/util/ContentUtils.kt
@@ -52,7 +52,9 @@ object ContentUtils {
         // var reason = "",
         // can capture the spoiler reason for better formatting? ex. { reason = it.value;  ">"}
         return formattedBody.replace("(?<=<span data-mx-spoiler)=\\\".+?\\\">".toRegex(), ">")
-            .replace("(?<=<span data-mx-spoiler>).+?(?=</span>)".toRegex(), { "█".repeat(it.value.length); })
-            .unescapeHtml()
+                .replace("(?<=<span data-mx-spoiler>).+?(?=</span>)".toRegex()) { SPOILER_CHAR.repeat(it.value.length); }
+                .unescapeHtml()
     }
+
+    private const val SPOILER_CHAR = "█"
 }
