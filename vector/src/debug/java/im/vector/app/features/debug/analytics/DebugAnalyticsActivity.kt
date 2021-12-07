9d4835ab25e6e954056871d5d1dab9e922443e3e
Benoit Marty <benoit@matrix.org>
2021-12-07 14:23:07 +0100

    Avoid mistake by getting the id using an existing view

--- vector/src/debug/java/im/vector/app/features/debug/analytics/DebugAnalyticsActivity.kt
+++ vector/src/debug/java/im/vector/app/features/debug/analytics/DebugAnalyticsActivity.kt
@@ -17,7 +17,6 @@
 package im.vector.app.features.debug.analytics
 
 import dagger.hilt.android.AndroidEntryPoint
-import im.vector.app.R
 import im.vector.app.core.extensions.addFragment
 import im.vector.app.core.platform.VectorBaseActivity
 import im.vector.app.databinding.ActivitySimpleBinding
@@ -30,7 +29,7 @@ class DebugAnalyticsActivity : VectorBaseActivity<ActivitySimpleBinding>() {
     override fun initUiAndData() {
         if (isFirstCreation()) {
             addFragment(
-                    R.id.simpleFragmentContainer,
+                    views.simpleFragmentContainer.id,
                     DebugAnalyticsFragment::class.java
             )
         }
