11f176e0798c96633400b9433542c8fcc269c52f
Benoit Marty <benoit@matrix.org>
2021-12-15 14:13:42 +0100

    Analytics: Create extension to compute JoinedRoom.RoomSize (#4716)

---
+++ vector/src/main/java/im/vector/app/features/analytics/extensions/AnalyticsExtensions.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.analytics.extensions
+
+import im.vector.app.features.analytics.plan.JoinedRoom
+
+fun Int.toAnalyticsRoomSize(): JoinedRoom.RoomSize {
+    return when (this) {
+        2            -> JoinedRoom.RoomSize.Two
+        in 3..10     -> JoinedRoom.RoomSize.ThreeToTen
+        in 11..100   -> JoinedRoom.RoomSize.ElevenToOneHundred
+        in 101..1000 -> JoinedRoom.RoomSize.OneHundredAndOneToAThousand
+        else         -> JoinedRoom.RoomSize.MoreThanAThousand
+    }
+}
