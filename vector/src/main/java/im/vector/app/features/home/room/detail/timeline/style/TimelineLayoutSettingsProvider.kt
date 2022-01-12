32e72f54b3906a7d95361f39452e0af5452916c9
ganfra <francoisg@element.io>
2022-01-12 19:01:13 +0100

    Bubbles: add quick settings (temporary)

--- vector/src/main/java/im/vector/app/features/home/room/detail/timeline/style/TimelineLayoutSettingsProvider.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/timeline/style/TimelineLayoutSettingsProvider.kt
@@ -16,11 +16,16 @@
 
 package im.vector.app.features.home.room.detail.timeline.style
 
+import im.vector.app.features.settings.VectorPreferences
 import javax.inject.Inject
 
-class TimelineLayoutSettingsProvider @Inject constructor() {
+class TimelineLayoutSettingsProvider @Inject constructor(private val vectorPreferences: VectorPreferences) {
 
     fun getLayoutSettings(): TimelineLayoutSettings {
-        return TimelineLayoutSettings.BUBBLE
+        return if (vectorPreferences.useMessageBubblesLayout()) {
+            TimelineLayoutSettings.BUBBLE
+        } else {
+            TimelineLayoutSettings.MODERN
+        }
     }
 }
