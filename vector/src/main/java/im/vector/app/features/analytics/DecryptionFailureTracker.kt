c0aa0cef62dff08865b29e44d833c06550ffc262
Benoit Marty <benoit@matrix.org>
2022-01-19 15:24:25 +0100

    Analytics: inject analyticsTracker, it has a better scope

--- vector/src/main/java/im/vector/app/features/analytics/DecryptionFailureTracker.kt
+++ vector/src/main/java/im/vector/app/features/analytics/DecryptionFailureTracker.kt
@@ -49,7 +49,7 @@ private const val CHECK_INTERVAL = 2_000L
  */
 @Singleton
 class DecryptionFailureTracker @Inject constructor(
-        private val vectorAnalytics: VectorAnalytics,
+        private val analyticsTracker: AnalyticsTracker,
         private val clock: Clock
 ) {
 
@@ -136,7 +136,7 @@ class DecryptionFailureTracker @Inject constructor(
                     // for now we ignore events already reported even if displayed again?
                     .filter { alreadyReported.contains(it).not() }
                     .forEach { failedEventId ->
-                        vectorAnalytics.capture(Error(failedEventId, Error.Domain.E2EE, aggregation.key))
+                        analyticsTracker.capture(Error(failedEventId, Error.Domain.E2EE, aggregation.key))
                         alreadyReported.add(failedEventId)
                     }
         }
