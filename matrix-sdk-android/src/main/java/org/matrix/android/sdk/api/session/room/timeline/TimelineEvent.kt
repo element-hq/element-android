ea9e5183dcb6277e072a9b5aab43f668ace0284d
Onuray Sahin <onuray.sahin@gmail.com>
2022-01-21 16:20:08 +0300

    Fix rendering edited polls in timeline.

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/timeline/TimelineEvent.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/timeline/TimelineEvent.kt
@@ -133,7 +133,7 @@ fun TimelineEvent.getEditedEventId(): String? {
 fun TimelineEvent.getLastMessageContent(): MessageContent? {
     return when (root.getClearType()) {
         EventType.STICKER    -> root.getClearContent().toModel<MessageStickerContent>()
-        EventType.POLL_START -> root.getClearContent().toModel<MessagePollContent>()
+        EventType.POLL_START -> (annotations?.editSummary?.latestContent ?: root.getClearContent()).toModel<MessagePollContent>()
         else                 -> (annotations?.editSummary?.latestContent ?: root.getClearContent()).toModel()
     }
 }
