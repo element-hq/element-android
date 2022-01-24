6c560679a59e9eaeeb5017774026e6bca4ec5562
Benoit Marty <benoit@matrix.org>
2022-01-24 18:49:20 +0100

    Hide the button to make the loading wheel centered

--- vector/src/main/java/im/vector/app/features/invite/InviteButtonStateBinder.kt
+++ vector/src/main/java/im/vector/app/features/invite/InviteButtonStateBinder.kt
@@ -16,7 +16,7 @@
 
 package im.vector.app.features.invite
 
-import androidx.core.view.isInvisible
+import androidx.core.view.isGone
 import im.vector.app.core.platform.ButtonStateView
 import org.matrix.android.sdk.api.session.room.members.ChangeMembershipState
 
@@ -38,7 +38,7 @@ object InviteButtonStateBinder {
         }
         // ButtonStateView.State.Loaded not used because roomSummary will not be displayed as a room invitation anymore
 
-        rejectView.isInvisible = requestInProgress
+        rejectView.isGone = requestInProgress
 
         when {
             changeMembershipState is ChangeMembershipState.FailedLeaving -> rejectView.render(ButtonStateView.State.Error)
