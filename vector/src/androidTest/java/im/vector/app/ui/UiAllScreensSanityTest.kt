76579eb80450e4bf99bfc9708a917c0cfb85f3ef
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-02-17 17:27:26 +0000

    Work on sd card

--- vector/src/androidTest/java/im/vector/app/ui/UiAllScreensSanityTest.kt
+++ vector/src/androidTest/java/im/vector/app/ui/UiAllScreensSanityTest.kt
@@ -16,10 +16,12 @@
 
 package im.vector.app.ui
 
+import android.Manifest
 import androidx.test.espresso.IdlingPolicies
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
 import im.vector.app.R
 import im.vector.app.espresso.tools.ScreenshotFailureRule
 import im.vector.app.features.MainActivity
