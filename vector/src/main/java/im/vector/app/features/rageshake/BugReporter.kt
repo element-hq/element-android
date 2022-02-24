85b59654aef10d085e51c824c58b8f703f73cbd0
Benoit Marty <benoit@matrix.org>
2022-02-24 22:25:20 +0100

    Remove unused strings in build.gradle. Rely on BuildConfig fields only

--- vector/src/main/java/im/vector/app/features/rageshake/BugReporter.kt
+++ vector/src/main/java/im/vector/app/features/rageshake/BugReporter.kt
@@ -271,7 +271,7 @@ class BugReporter @Inject constructor(
                             .addFormDataPart("can_contact", canContact.toString())
                             .addFormDataPart("device_id", deviceId)
                             .addFormDataPart("version", versionProvider.getVersion(longFormat = true, useBuildNumber = false))
-                            .addFormDataPart("branch_name", context.getString(R.string.git_branch_name))
+                            .addFormDataPart("branch_name", BuildConfig.GIT_BRANCH_NAME)
                             .addFormDataPart("matrix_sdk_version", Matrix.getSdkVersion())
                             .addFormDataPart("olm_version", olmVersion)
                             .addFormDataPart("device", Build.MODEL.trim())
@@ -289,7 +289,7 @@ class BugReporter @Inject constructor(
                                 }
                             }
 
-                    val buildNumber = context.getString(R.string.build_number)
+                    val buildNumber = BuildConfig.BUILD_NUMBER
                     if (buildNumber.isNotEmpty() && buildNumber != "0") {
                         builder.addFormDataPart("build_number", buildNumber)
                     }
@@ -329,7 +329,7 @@ class BugReporter @Inject constructor(
                     // add some github labels
                     builder.addFormDataPart("label", BuildConfig.VERSION_NAME)
                     builder.addFormDataPart("label", BuildConfig.FLAVOR_DESCRIPTION)
-                    builder.addFormDataPart("label", context.getString(R.string.git_branch_name))
+                    builder.addFormDataPart("label", BuildConfig.GIT_BRANCH_NAME)
 
                     // Special for Element
                     builder.addFormDataPart("label", "[Element]")
