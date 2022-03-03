3c3da511ec15b69a34a2f2e2d5946ef889bdc575
akallabeth <akallabeth@posteo.net>
2022-03-03 12:29:38 +0100

    Run ImageUtils requests async
    Fixes #5236: Run file requests in background to not block ui thread

--- library/multipicker/src/main/java/im/vector/lib/multipicker/utils/ImageUtils.kt
+++ library/multipicker/src/main/java/im/vector/lib/multipicker/utils/ImageUtils.kt
@@ -23,35 +23,51 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import androidx.exifinterface.media.ExifInterface
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 
 object ImageUtils {
 
     fun getBitmap(context: Context, uri: Uri): Bitmap? {
-        return try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
-            } else {
-                context.contentResolver.openInputStream(uri)?.use { inputStream ->
-                    BitmapFactory.decodeStream(inputStream)
+        var bitmap: Bitmap? = null
+        runBlocking {
+            val job = async {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
+                    } else {
+                        context.contentResolver.openInputStream(uri)?.use { inputStream ->
+                            BitmapFactory.decodeStream(inputStream)
+                        }
+                    }
+                } catch (e: Exception) {
+                    Timber.e(e, "Cannot decode Bitmap: %s", uri.toString())
+                    null
                 }
             }
-        } catch (e: Exception) {
-            Timber.e(e, "Cannot decode Bitmap: %s", uri.toString())
-            null
+            bitmap = job.await()
         }
+
+        return bitmap
     }
 
     fun getOrientation(context: Context, uri: Uri): Int {
         var orientation = 0
-        context.contentResolver.openInputStream(uri)?.use { inputStream ->
-            try {
-                ExifInterface(inputStream).let {
-                    orientation = it.rotationDegrees
-                }
-            } catch (e: Exception) {
-                Timber.e(e, "Cannot read orientation: %s", uri.toString())
+        runBlocking {
+            val job = async {
+                context.contentResolver.openInputStream(uri)?.use {
+                    inputStream ->
+                        try {
+                            ExifInterface(inputStream).let {
+                                orientation = it.rotationDegrees
+                            }
+                        } catch (e: Exception) {
+                            Timber.e(e, "Cannot read orientation: %s", uri.toString())
+                        }
+                    }
             }
+            job.await()
         }
         return orientation
     }
