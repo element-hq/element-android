aae75ce52fa1541b32f54ee96a3442b65a92844a
Onuray Sahin <onuray.sahin@gmail.com>
2022-03-04 16:54:56 +0300

    Always stop all voice actions and media player if app enters to the background.

--- vector/src/main/java/im/vector/app/features/home/room/detail/composer/MessageComposerViewModel.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/composer/MessageComposerViewModel.kt
@@ -872,11 +872,14 @@ class MessageComposerViewModel @AssistedInject constructor(
     }
 
     private fun handleEntersBackground(composerText: String) {
+        // Always stop all voice actions. It may be playing in timeline or active recording
+        val playingAudioContent = voiceMessageHelper.stopAllVoiceActions(deleteRecord = false)
+        voiceMessageHelper.clearTracker()
+        
         val isVoiceRecording = com.airbnb.mvrx.withState(this) { it.isVoiceRecording }
         if (isVoiceRecording) {
-            voiceMessageHelper.clearTracker()
             viewModelScope.launch {
-                voiceMessageHelper.stopAllVoiceActions(deleteRecord = false)?.toContentAttachmentData()?.let { voiceDraft ->
+                playingAudioContent?.toContentAttachmentData()?.let { voiceDraft ->
                     val content = voiceDraft.toJsonString()
                     room.saveDraft(UserDraft.Voice(content))
                     setState { copy(sendMode = SendMode.Voice(content)) }
