bfd31de05ec3250a8e14c9f105491cfbd54445bb
ericdecanini <eddecanini@gmail.com>
2022-03-22 17:56:07 +0100

    Renames call option to be more agnostic

--- vector/src/main/java/im/vector/app/features/home/room/detail/RoomDetailViewState.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/RoomDetailViewState.kt
@@ -87,7 +87,7 @@ data class RoomDetailViewState(
             rootThreadEventId = args.threadTimelineArgs?.rootThreadEventId
     )
 
-    fun isWebRTCCallOptionAvailable() = asyncRoomSummary.invoke()?.isDirect ?: true
+    fun isCallOptionAvailable() = asyncRoomSummary.invoke()?.isDirect ?: true
 
     fun isSearchAvailable() = asyncRoomSummary()?.isEncrypted == false
 
