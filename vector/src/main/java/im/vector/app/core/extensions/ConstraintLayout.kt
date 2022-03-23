2cbbfca73fdffba98d4acc771592f4f23d02b5c2
Adam Brown <adampsbrown@gmail.com>
2022-03-23 16:27:42 +0000

    adding helper for recalculating percentage heights within a constraint layout - this allows percentages to be used which make of the screen viewport rather than the accumulated scroll height

--- vector/src/main/java/im/vector/app/core/extensions/ConstraintLayout.kt
+++ vector/src/main/java/im/vector/app/core/extensions/ConstraintLayout.kt
@@ -16,13 +16,30 @@
 
 package im.vector.app.core.extensions
 
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.children
+import androidx.core.view.doOnLayout
+import kotlin.math.roundToInt
 
 fun ConstraintLayout.updateConstraintSet(block: (ConstraintSet) -> Unit) {
     ConstraintSet().let {
         it.clone(this)
         block.invoke(it)
         it.applyTo(this)
+    }
+}
+
+fun ConstraintLayout.realignPercentagesToParent() {
+    doOnLayout {
+        val rootHeight = (parent as View).height
+        children.forEach { child ->
+            val params = child.layoutParams as ConstraintLayout.LayoutParams
+            if (params.matchConstraintPercentHeight != 1.0f) {
+                params.height = (rootHeight * params.matchConstraintPercentHeight).roundToInt()
+                child.layoutParams = params
+            }
+        }
     }
 }
