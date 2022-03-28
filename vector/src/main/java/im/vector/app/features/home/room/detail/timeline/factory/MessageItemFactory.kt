8aaaf80262a26ca7b02852386a11cb88ca685d37
ericdecanini <eddecanini@gmail.com>
2022-03-28 10:47:59 +0200

    Fixes lint error

--- vector/src/main/java/im/vector/app/features/home/room/detail/timeline/factory/MessageItemFactory.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/timeline/factory/MessageItemFactory.kt
@@ -492,8 +492,7 @@ class MessageItemFactory @Inject constructor(
             informationData: MessageInformationData,
             highlight: Boolean,
             callback: TimelineEventController.Callback?,
-            attributes: AbsMessageItem.Attributes)
-    : MessageTextItem? {
+            attributes: AbsMessageItem.Attributes): MessageTextItem? {
         // For compatibility reason we should display the body
         return buildMessageTextItem(messageContent.body, false, informationData, highlight, callback, attributes)
     }
