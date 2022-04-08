e2d56e2d87ced2e75736e6327de81ecfed3174a7
Maxime NATUREL <maxime.naturel@niji.fr>
2022-04-08 10:20:24 +0200

    Align corner radius of banner on map view

--- vector/src/main/java/im/vector/app/features/home/room/detail/timeline/item/MessageLiveLocationStartItem.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/timeline/item/MessageLiveLocationStartItem.kt
@@ -16,16 +16,19 @@
 
 package im.vector.app.features.home.room.detail.timeline.item
 
+import android.graphics.drawable.ColorDrawable
 import android.widget.ImageView
 import androidx.core.view.updateLayoutParams
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import im.vector.app.R
 import im.vector.app.core.glide.GlideApp
 import im.vector.app.core.utils.DimensionConverter
 import im.vector.app.features.home.room.detail.timeline.style.TimelineMessageLayout
 import im.vector.app.features.home.room.detail.timeline.style.granularRoundedCorners
+import im.vector.app.features.themes.ThemeUtils
 
 @EpoxyModelClass(layout = R.layout.item_timeline_event_base)
 abstract class MessageLiveLocationStartItem : AbsMessageItem<MessageLiveLocationStartItem.Holder>() {
@@ -39,13 +42,16 @@ abstract class MessageLiveLocationStartItem : AbsMessageItem<MessageLiveLocation
     override fun bind(holder: Holder) {
         super.bind(holder)
         renderSendState(holder.view, null)
+        bindMap(holder)
+        bindBottomBanner(holder)
+    }
 
+    private fun bindMap(holder: Holder) {
         val messageLayout = attributes.informationData.messageLayout
-        val dimensionConverter = DimensionConverter(holder.view.resources)
-        val imageCornerTransformation = if (messageLayout is TimelineMessageLayout.Bubble) {
+        val mapCornerTransformation = if (messageLayout is TimelineMessageLayout.Bubble) {
             messageLayout.cornersRadius.granularRoundedCorners()
         } else {
-            RoundedCorners(dimensionConverter.dpToPx(8))
+            RoundedCorners(getDefaultLayoutCornerRadiusInDp(holder))
         }
         holder.noLocationMapImageView.updateLayoutParams {
             width = mapWidth
@@ -53,13 +59,33 @@ abstract class MessageLiveLocationStartItem : AbsMessageItem<MessageLiveLocation
         }
         GlideApp.with(holder.noLocationMapImageView)
                 .load(R.drawable.bg_no_location_map)
-                .transform(imageCornerTransformation)
+                .transform(mapCornerTransformation)
                 .into(holder.noLocationMapImageView)
+    }
+
+    private fun bindBottomBanner(holder: Holder) {
+        val messageLayout = attributes.informationData.messageLayout
+        val imageCornerTransformation = if (messageLayout is TimelineMessageLayout.Bubble) {
+            GranularRoundedCorners(0f, 0f, messageLayout.cornersRadius.bottomEndRadius, messageLayout.cornersRadius.bottomStartRadius)
+        } else {
+            val bottomCornerRadius = getDefaultLayoutCornerRadiusInDp(holder).toFloat()
+            GranularRoundedCorners(0f, 0f, bottomCornerRadius, bottomCornerRadius)
+        }
+        GlideApp.with(holder.bannerImageView)
+                .load(ColorDrawable(ThemeUtils.getColor(holder.bannerImageView.context, R.attr.colorSurface)))
+                .transform(imageCornerTransformation)
+                .into(holder.bannerImageView)
+    }
+
+    private fun getDefaultLayoutCornerRadiusInDp(holder: Holder): Int {
+        val dimensionConverter = DimensionConverter(holder.view.resources)
+        return dimensionConverter.dpToPx(8)
     }
 
     override fun getViewStubId() = STUB_ID
 
     class Holder : AbsMessageItem.Holder(STUB_ID) {
+        val bannerImageView by bind<ImageView>(R.id.locationLiveStartBanner)
         val noLocationMapImageView by bind<ImageView>(R.id.locationLiveStartMap)
     }
 
