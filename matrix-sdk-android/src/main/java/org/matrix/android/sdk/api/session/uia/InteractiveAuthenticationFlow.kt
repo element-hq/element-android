b5654d54b9bc2f5232a1d351c9bc4a0de4b44d7c
Benoit Marty <benoit@matrix.org>
2022-04-11 18:50:15 +0200

    Move `InteractiveAuthenticationFlow` to api package

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/uia/InteractiveAuthenticationFlow.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/uia/InteractiveAuthenticationFlow.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.matrix.android.sdk.internal.auth.data
+package org.matrix.android.sdk.api.session.uia
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
