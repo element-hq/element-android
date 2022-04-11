d0a4075238ccf5ed142867bda55ce046c8c7b454
Benoit Marty <benoit@matrix.org>
2022-04-11 19:15:31 +0200

    Move `TaggedEventsContent` to api package

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/taggedevents/TaggedEventsContent.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/taggedevents/TaggedEventsContent.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.matrix.android.sdk.internal.session.room.taggedevents
+package org.matrix.android.sdk.api.session.room.taggedevents
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
