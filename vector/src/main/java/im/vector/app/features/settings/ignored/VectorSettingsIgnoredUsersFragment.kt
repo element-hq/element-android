0fbad92e8bcefa4e5141a38ebd8764da591d423e
Benoit Marty <benoit@matrix.org>
2022-04-15 13:06:53 +0200

    ktlint

--- vector/src/main/java/im/vector/app/features/settings/ignored/VectorSettingsIgnoredUsersFragment.kt
+++ vector/src/main/java/im/vector/app/features/settings/ignored/VectorSettingsIgnoredUsersFragment.kt
@@ -30,8 +30,6 @@ import im.vector.app.core.extensions.cleanup
 import im.vector.app.core.extensions.configureWith
 import im.vector.app.core.platform.VectorBaseFragment
 import im.vector.app.databinding.FragmentGenericRecyclerBinding
-import im.vector.app.features.MainActivity
-import im.vector.app.features.MainActivityArgs
 import im.vector.app.features.analytics.plan.MobileScreen
 import javax.inject.Inject
 
