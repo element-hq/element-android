ff386c3de6cbc308a3e7ceed6cddc34db9edcd06
Benoit Marty <benoit@matrix.org>
2022-05-11 10:22:07 +0200

    Fix lint internal issue: remove `object Params`. GetTurnServerTask.kt: Error: Unexpected failure during lint analysis of GetTurnServerTask.kt (this is a bug in lint or one of the libraries it depends on)

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/call/TurnServerDataSource.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/call/TurnServerDataSource.kt
@@ -39,7 +39,7 @@ internal class TurnServerDataSource @Inject constructor(private val turnServerTa
     }
 
     suspend fun getTurnServer(): TurnServerResponse {
-        return cachedTurnServerResponse.data ?: turnServerTask.execute(GetTurnServerTask.Params).also {
+        return cachedTurnServerResponse.data ?: turnServerTask.execute(Unit).also {
             cachedTurnServerResponse.data = it
         }
     }
