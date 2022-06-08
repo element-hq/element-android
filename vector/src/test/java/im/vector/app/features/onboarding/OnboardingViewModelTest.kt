7971a7461472ed0cb430a37db22776196bfe5c1d
Adam Brown <adampsbrown@gmail.com>
2022-06-08 11:40:55 +0100

    fixing code quality warnings

--- vector/src/test/java/im/vector/app/features/onboarding/OnboardingViewModelTest.kt
+++ vector/src/test/java/im/vector/app/features/onboarding/OnboardingViewModelTest.kt
@@ -559,7 +559,6 @@ class OnboardingViewModelTest {
                 .finish()
     }
 
-
     private fun viewModelWith(state: OnboardingViewState) {
         OnboardingViewModel(
                 state,
