eb84072a05ccc58704eabb1e09befeabc706d3b8
Adam Brown <adampsbrown@gmail.com>
2022-06-08 11:34:45 +0100

    updating test names to read better

--- vector/src/test/java/im/vector/app/features/onboarding/OnboardingViewModelTest.kt
+++ vector/src/test/java/im/vector/app/features/onboarding/OnboardingViewModelTest.kt
@@ -500,7 +500,7 @@ class OnboardingViewModelTest {
     }
 
     @Test
-    fun `given reset state, when resending reset password email, then triggers reset password and emits nothing`() = runTest {
+    fun `given existing reset state, when resending reset password email, then triggers reset password and emits nothing`() = runTest {
         viewModelWith(initialState.copy(resetState = ResetState(AN_EMAIL, A_PASSWORD)))
         val test = viewModel.test()
         fakeLoginWizard.givenResetPasswordSuccess(AN_EMAIL)
@@ -520,7 +520,7 @@ class OnboardingViewModelTest {
     }
 
     @Test
-    fun `given can successfully confirm reset password, when confirm reset password, then opens reset password complete`() = runTest {
+    fun `given combined login disabled, when confirming password reset, then opens reset password complete`() = runTest {
         viewModelWith(initialState.copy(resetState = ResetState(AN_EMAIL, A_PASSWORD)))
         val test = viewModel.test()
         fakeVectorFeatures.givenCombinedLoginDisabled()
@@ -540,7 +540,7 @@ class OnboardingViewModelTest {
     }
 
     @Test
-    fun `given can successfully confirm reset password, when confirm reset password, then emits reset password complete`() = runTest {
+    fun `given combined login enabled, when confirming password reset, then emits reset password complete`() = runTest {
         viewModelWith(initialState.copy(resetState = ResetState(AN_EMAIL, A_PASSWORD)))
         val test = viewModel.test()
         fakeVectorFeatures.givenCombinedLoginEnabled()
