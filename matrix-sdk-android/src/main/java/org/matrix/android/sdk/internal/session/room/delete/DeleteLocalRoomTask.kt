0d9cd2b3a3bbf4e2b49fc3ff01102a548bd16e81
Florian Renaud <florianr@element.io>
2022-07-04 17:47:48 +0200

    Delete local room related entities with cascade

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/room/delete/DeleteLocalRoomTask.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/room/delete/DeleteLocalRoomTask.kt
@@ -25,6 +25,7 @@ import org.matrix.android.sdk.internal.database.model.RoomEntity
 import org.matrix.android.sdk.internal.database.model.RoomMemberSummaryEntity
 import org.matrix.android.sdk.internal.database.model.RoomSummaryEntity
 import org.matrix.android.sdk.internal.database.model.TimelineEventEntity
+import org.matrix.android.sdk.internal.database.model.deleteOnCascade
 import org.matrix.android.sdk.internal.database.query.where
 import org.matrix.android.sdk.internal.database.query.whereRoomId
 import org.matrix.android.sdk.internal.di.SessionDatabase
@@ -59,10 +60,10 @@ internal class DefaultDeleteLocalRoomTask @Inject constructor(
                         ?.deleteAllFromRealm()
                 TimelineEventEntity.whereRoomId(realm, roomId = roomId).findAll()
                         ?.also { Timber.i("## DeleteLocalRoomTask - TimelineEventEntity - delete ${it.size} entries") }
-                        ?.deleteAllFromRealm()
+                        ?.forEach { it.deleteOnCascade(true) }
                 ChunkEntity.where(realm, roomId = roomId).findAll()
                         ?.also { Timber.i("## DeleteLocalRoomTask - ChunkEntity - delete ${it.size} entries") }
-                        ?.deleteAllFromRealm()
+                        ?.forEach { it.deleteOnCascade(deleteStateEvents = true, canDeleteRoot = true) }
                 RoomSummaryEntity.where(realm, roomId = roomId).findAll()
                         ?.also { Timber.i("## DeleteLocalRoomTask - RoomSummaryEntity - delete ${it.size} entries") }
                         ?.deleteAllFromRealm()
