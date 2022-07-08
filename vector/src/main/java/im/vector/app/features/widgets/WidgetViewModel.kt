85b5713b244775f535c37e2274836d5b4e9fcf7a
Onuray Sahin <onuray.sahin@gmail.com>
2022-07-08 15:06:44 +0300

    Add a hangup button in pip mode.

--- vector/src/main/java/im/vector/app/features/widgets/WidgetViewModel.kt
+++ vector/src/main/java/im/vector/app/features/widgets/WidgetViewModel.kt
@@ -147,7 +147,12 @@ class WidgetViewModel @AssistedInject constructor(
             WidgetAction.DeleteWidget -> handleDeleteWidget()
             WidgetAction.RevokeWidget -> handleRevokeWidget()
             WidgetAction.OnTermsReviewed -> loadFormattedUrl(forceFetchToken = false)
+            WidgetAction.HangupElementCall -> handleHangupElementCall()
         }
+    }
+
+    private fun handleHangupElementCall() {
+        _viewEvents.post(WidgetViewEvents.Close())
     }
 
     private fun handleRevokeWidget() {
