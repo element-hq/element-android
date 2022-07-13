82391aa28128cacf9b67572fcdd58a75cf7f275e
Maxime NATUREL <maxime.naturel@niji.fr>
2022-07-13 13:48:54 +0200

    Replacing ViewEvent by a ViewState property

--- vector/src/main/java/im/vector/app/features/home/room/detail/TimelineViewModel.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/TimelineViewModel.kt
@@ -1295,11 +1295,11 @@ class TimelineViewModel @AssistedInject constructor(
     }
 
     override fun onLocationServiceRunning() {
-        _viewEvents.post(RoomDetailViewEvents.ChangeLocationIndicator(isVisible = true))
+        setState { copy(isSharingLiveLocation = true) }
     }
 
     override fun onLocationServiceStopped() {
-        _viewEvents.post(RoomDetailViewEvents.ChangeLocationIndicator(isVisible = false))
+        setState { copy(isSharingLiveLocation = false) }
         // Bind again in case user decides to share live location without leaving the room
         locationSharingServiceConnection.bind(this)
     }
