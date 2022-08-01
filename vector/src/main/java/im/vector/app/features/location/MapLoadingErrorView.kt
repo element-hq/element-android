6972622bc6a685a2598c8bd9a7cbcd7f4b8c4b72
Maxime NATUREL <maxime.naturel@niji.fr>
2022-08-01 14:45:08 +0200

    Handling map loading error in timeline

--- vector/src/main/java/im/vector/app/features/location/MapLoadingErrorView.kt
+++ vector/src/main/java/im/vector/app/features/location/MapLoadingErrorView.kt
@@ -17,10 +17,16 @@
 package im.vector.app.features.location
 
 import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.use
+import im.vector.app.R
+import im.vector.app.core.glide.GlideApp
 import im.vector.app.databinding.ViewMapLoadingErrorBinding
+import im.vector.app.features.themes.ThemeUtils
 
 /**
  * Custom view to display an error when map fails to load.
@@ -29,10 +35,35 @@ class MapLoadingErrorView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
+    private val binding = ViewMapLoadingErrorBinding.inflate(
+            LayoutInflater.from(context),
+            this
+    )
+
     init {
-        ViewMapLoadingErrorBinding.inflate(
-                LayoutInflater.from(context),
-                this
-        )
+        context.obtainStyledAttributes(
+                attrs,
+                R.styleable.MapLoadingErrorView,
+                0,
+                0
+        ).use {
+            setErrorDescription(it)
+        }
+    }
+
+    private fun setErrorDescription(typedArray: TypedArray) {
+        val description = typedArray.getString(R.styleable.MapLoadingErrorView_mapErrorDescription)
+        if(description.isNullOrEmpty()) {
+            binding.mapLoadingErrorDescription.setText(R.string.location_share_loading_map_error)
+        } else {
+            binding.mapLoadingErrorDescription.text = description
+        }
+    }
+
+    fun render(mapLoadingErrorViewState: MapLoadingErrorViewState) {
+        GlideApp.with(binding.mapLoadingErrorBackground)
+                .load(ColorDrawable(ThemeUtils.getColor(context, R.attr.vctr_system)))
+                .transform(mapLoadingErrorViewState.backgroundTransformation)
+                .into(binding.mapLoadingErrorBackground)
     }
 }
