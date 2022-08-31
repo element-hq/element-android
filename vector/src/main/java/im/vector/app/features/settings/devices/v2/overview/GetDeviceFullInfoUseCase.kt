13626a161aaf06c7348021fbeabf04cfe1f700eb
Maxime NATUREL <maxime.naturel@niji.fr>
2022-08-31 10:09:40 +0200

    Adding use case to get full device info for a given device id

---
+++ vector/src/main/java/im/vector/app/features/settings/devices/v2/overview/GetDeviceFullInfoUseCase.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.settings.devices.v2.overview
+
+import androidx.lifecycle.asFlow
+import im.vector.app.core.di.ActiveSessionHolder
+import im.vector.app.features.settings.devices.DeviceFullInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
+import org.matrix.android.sdk.api.util.Optional
+import org.matrix.android.sdk.api.util.toOptional
+import javax.inject.Inject
+
+// TODO add unit tests
+class GetDeviceFullInfoUseCase @Inject constructor(
+        private val activeSessionHolder: ActiveSessionHolder,
+) {
+
+    fun execute(deviceId: String): Flow<Optional<DeviceFullInfo>> {
+        return activeSessionHolder.getSafeActiveSession()?.let { session ->
+            combine(
+                    session.cryptoService().getMyDevicesInfoLive(deviceId).asFlow(),
+                    session.cryptoService().getLiveCryptoDeviceInfoWithId(deviceId).asFlow()
+            ) { deviceInfo, cryptoDeviceInfo ->
+                val info = deviceInfo.getOrNull()
+                val cryptoInfo = cryptoDeviceInfo.getOrNull()
+                val fullInfo = if (info != null && cryptoInfo != null) {
+                    DeviceFullInfo(
+                            deviceInfo = info,
+                            cryptoDeviceInfo = cryptoInfo
+                    )
+                } else {
+                    null
+                }
+                fullInfo.toOptional()
+            }
+        } ?: emptyFlow()
+    }
+}
