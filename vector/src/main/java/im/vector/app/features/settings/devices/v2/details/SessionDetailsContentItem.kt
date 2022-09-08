94d1649ec9a600060a91310747d5f70c667bff4c
Maxime NATUREL <maxime.naturel@niji.fr>
2022-09-08 15:10:48 +0200

    Creation of header and content items

---
+++ vector/src/main/java/im/vector/app/features/settings/devices/v2/details/SessionDetailsContentItem.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.settings.devices.v2.details
+
+import android.view.View
+import android.widget.TextView
+import com.airbnb.epoxy.EpoxyAttribute
+import com.airbnb.epoxy.EpoxyModelClass
+import im.vector.app.R
+import im.vector.app.core.epoxy.VectorEpoxyHolder
+import im.vector.app.core.epoxy.VectorEpoxyModel
+
+@EpoxyModelClass
+abstract class SessionDetailsContentItem : VectorEpoxyModel<SessionDetailsContentItem.Holder>(R.layout.item_session_details_header) {
+
+    @EpoxyAttribute
+    var title: String? = null
+
+    @EpoxyAttribute
+    var description: String? = null
+
+    @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
+    var onLongClickListener: View.OnLongClickListener? = null
+
+    override fun bind(holder: Holder) {
+        super.bind(holder)
+        holder.sessionDetailsContentTitle.text = title
+        holder.sessionDetailsContentDescription.text = description
+        holder.view.isClickable = onLongClickListener != null
+        holder.view.setOnLongClickListener(onLongClickListener)
+    }
+
+    class Holder : VectorEpoxyHolder() {
+        val sessionDetailsContentTitle by bind<TextView>(R.id.sessionDetailsContentTitle)
+        val sessionDetailsContentDescription by bind<TextView>(R.id.sessionDetailsContentDescription)
+    }
+}
---
+++ vector/src/main/java/im/vector/app/features/settings/devices/v2/details/SessionDetailsHeaderItem.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.settings.devices.v2.details
+
+import android.widget.TextView
+import com.airbnb.epoxy.EpoxyAttribute
+import com.airbnb.epoxy.EpoxyModelClass
+import im.vector.app.R
+import im.vector.app.core.epoxy.VectorEpoxyHolder
+import im.vector.app.core.epoxy.VectorEpoxyModel
+
+@EpoxyModelClass
+abstract class SessionDetailsHeaderItem : VectorEpoxyModel<SessionDetailsHeaderItem.Holder>(R.layout.item_session_details_header) {
+
+    @EpoxyAttribute
+    var title: String? = null
+
+    override fun bind(holder: Holder) {
+        super.bind(holder)
+        holder.sessionDetailsHeaderTitle.text = title
+    }
+
+    class Holder : VectorEpoxyHolder() {
+        val sessionDetailsHeaderTitle by bind<TextView>(R.id.sessionDetailsHeaderTitle)
+    }
+}
