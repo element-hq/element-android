7c4527fba645cc8b0ccc93ea962655da148ca6f9
Benoit Marty <benoit@matrix.org>
2022-09-09 15:58:16 +0200

    Fix test - no need to use Moshi to parse FCM Push data.

--- vector/src/main/java/im/vector/app/core/pushers/PushParser.kt
+++ vector/src/main/java/im/vector/app/core/pushers/PushParser.kt
@@ -20,27 +20,26 @@ import im.vector.app.core.pushers.model.PushData
 import im.vector.app.core.pushers.model.PushDataFcm
 import im.vector.app.core.pushers.model.PushDataUnifiedPush
 import im.vector.app.core.pushers.model.toPushData
-import org.json.JSONObject
 import org.matrix.android.sdk.api.extensions.tryOrNull
 import org.matrix.android.sdk.api.util.MatrixJsonParser
 import javax.inject.Inject
 
+/**
+ * Parse the received data from Push. Json format are different depending on the source.
+ *
+ * Notifications received by FCM are formatted by the matrix gateway [1]. The data send to FCM is the content
+ * of the "notification" attribute of the json sent to the gateway [2][3].
+ * On the other side, with UnifiedPush, the content of the message received is the content posted to the push
+ * gateway endpoint [3].
+ *
+ * *Note*: If we want to get the same content with FCM and unifiedpush, we can do a new sygnal pusher [4].
+ *
+ * [1] https://github.com/matrix-org/sygnal/blob/main/sygnal/gcmpushkin.py
+ * [2] https://github.com/matrix-org/sygnal/blob/main/sygnal/gcmpushkin.py#L366
+ * [3] https://spec.matrix.org/latest/push-gateway-api/
+ * [4] https://github.com/p1gp1g/sygnal/blob/unifiedpush/sygnal/upfcmpushkin.py (Not tested for a while)
+ */
 class PushParser @Inject constructor() {
-    /**
-     * Parse the received data from Push. Json format are different depending on the source.
-     *
-     * Notifications received by FCM are formatted by the matrix gateway [1]. The data send to FCM is the content
-     * of the "notification" attribute of the json sent to the gateway [2][3].
-     * On the other side, with UnifiedPush, the content of the message received is the content posted to the push
-     * gateway endpoint [3].
-     *
-     * *Note*: If we want to get the same content with FCM and unifiedpush, we can do a new sygnal pusher [4].
-     *
-     * [1] https://github.com/matrix-org/sygnal/blob/main/sygnal/gcmpushkin.py
-     * [2] https://github.com/matrix-org/sygnal/blob/main/sygnal/gcmpushkin.py#L366
-     * [3] https://spec.matrix.org/latest/push-gateway-api/
-     * [4] https://github.com/p1gp1g/sygnal/blob/unifiedpush/sygnal/upfcmpushkin.py (Not tested for a while)
-     */
     fun parsePushDataUnifiedPush(message: ByteArray): PushData? {
         return MatrixJsonParser.getMoshi().let {
             tryOrNull { it.adapter(PushDataUnifiedPush::class.java).fromJson(String(message)) }?.toPushData()
@@ -48,8 +47,16 @@ class PushParser @Inject constructor() {
     }
 
     fun parsePushDataFcm(message: Map<*, *>): PushData? {
-        return MatrixJsonParser.getMoshi().let {
-            tryOrNull { it.adapter(PushDataFcm::class.java).fromJson(JSONObject(message).toString()) }?.toPushData()
+        if ((message.containsKey("event_id") && message["event_id"] !is String) ||
+                message.containsKey("room_id") && message["room_id"] !is String ||
+                message.containsKey("unread") && message["unread"] !is Int) {
+            return null
         }
+        val pushDataFcm = PushDataFcm(
+                eventId = message["event_id"] as String?,
+                roomId = message["room_id"] as String?,
+                unread = message["unread"] as Int?,
+        )
+        return pushDataFcm.toPushData()
     }
 }
