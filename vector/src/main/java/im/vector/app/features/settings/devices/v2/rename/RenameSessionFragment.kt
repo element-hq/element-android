1091ae41ad95c41bb5aa6f7331081c3b269c0a5d
Maxime NATUREL <maxime.naturel@niji.fr>
2022-09-19 11:18:49 +0200

    Default structure for the new code

---
+++ vector/src/main/java/im/vector/app/features/settings/devices/v2/rename/RenameSessionFragment.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.settings.devices.v2.rename
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.airbnb.mvrx.fragmentViewModel
+import com.airbnb.mvrx.withState
+import dagger.hilt.android.AndroidEntryPoint
+import im.vector.app.core.platform.VectorBaseFragment
+import im.vector.app.databinding.FragmentSessionRenameBinding
+import javax.inject.Inject
+
+/**
+ * Display the screen to rename a Session.
+ */
+@AndroidEntryPoint
+class RenameSessionFragment :
+        VectorBaseFragment<FragmentSessionRenameBinding>() {
+
+    private val viewModel: RenameSessionViewModel by fragmentViewModel()
+
+    @Inject lateinit var viewNavigator: RenameSessionViewNavigator
+
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentSessionRenameBinding {
+        return FragmentSessionRenameBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewEvents()
+    }
+
+    private fun observeViewEvents() {
+        viewModel.observeViewEvents {
+            when (it) {
+                is RenameSessionViewEvent.SessionRenamed -> {
+                    viewNavigator.navigateBack(requireActivity())
+                }
+            }
+        }
+    }
+
+    override fun invalidate() = withState(viewModel) { _ ->
+        // TODO
+    }
+}
---
+++ vector/src/main/java/im/vector/app/features/settings/devices/v2/rename/RenameSessionViewModel.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.settings.devices.v2.rename
+
+import com.airbnb.mvrx.MavericksViewModelFactory
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import im.vector.app.core.di.MavericksAssistedViewModelFactory
+import im.vector.app.core.di.hiltMavericksViewModelFactory
+import im.vector.app.core.platform.EmptyAction
+import im.vector.app.core.platform.VectorViewModel
+import im.vector.app.features.settings.devices.v2.overview.GetDeviceFullInfoUseCase
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+// TODO add unit tests
+class RenameSessionViewModel @AssistedInject constructor(
+        @Assisted val initialState: RenameSessionViewState,
+        private val getDeviceFullInfoUseCase: GetDeviceFullInfoUseCase,
+) : VectorViewModel<RenameSessionViewState, EmptyAction, RenameSessionViewEvent>(initialState) {
+
+    companion object : MavericksViewModelFactory<RenameSessionViewModel, RenameSessionViewState> by hiltMavericksViewModelFactory()
+
+    @AssistedFactory
+    interface Factory : MavericksAssistedViewModelFactory<RenameSessionViewModel, RenameSessionViewState> {
+        override fun create(initialState: RenameSessionViewState): RenameSessionViewModel
+    }
+
+    init {
+        observeSessionInfo(initialState.deviceId)
+    }
+
+    private fun observeSessionInfo(deviceId: String) {
+        getDeviceFullInfoUseCase.execute(deviceId)
+                .onEach { setState { copy(deviceName = it.deviceInfo.displayName.orEmpty()) } }
+                .launchIn(viewModelScope)
+    }
+
+    override fun handle(action: EmptyAction) {
+        // do nothing
+    }
+}
