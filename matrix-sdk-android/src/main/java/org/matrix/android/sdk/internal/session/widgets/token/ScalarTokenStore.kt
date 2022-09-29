981ab5bc87b4608289a88256a34ab285df29e6fe
ganfra <francoisg@element.io>
2022-09-29 18:42:41 +0200

    Realm kotlin: migrate ScalarTokenStore

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/widgets/token/ScalarTokenStore.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/widgets/token/ScalarTokenStore.kt
@@ -16,35 +16,38 @@
 
 package org.matrix.android.sdk.internal.session.widgets.token
 
-import com.zhuinden.monarchy.Monarchy
-import io.realm.kotlin.deleteFromRealm
+import io.realm.kotlin.UpdatePolicy
+import org.matrix.android.sdk.internal.database.RealmInstance
 import org.matrix.android.sdk.internal.database.model.ScalarTokenEntity
 import org.matrix.android.sdk.internal.database.query.where
 import org.matrix.android.sdk.internal.di.SessionDatabase
-import org.matrix.android.sdk.internal.util.awaitTransaction
-import org.matrix.android.sdk.internal.util.fetchCopyMap
 import javax.inject.Inject
 
-internal class ScalarTokenStore @Inject constructor(@SessionDatabase private val monarchy: Monarchy) {
+internal class ScalarTokenStore @Inject constructor(@SessionDatabase private val realmInstance: RealmInstance) {
 
     fun getToken(apiUrl: String): String? {
-        return monarchy.fetchCopyMap({ realm ->
-            ScalarTokenEntity.where(realm, apiUrl).findFirst()
-        }, { scalarToken, _ ->
-            scalarToken.token
-        })
+        val realm = realmInstance.getBlockingRealm()
+        return ScalarTokenEntity.where(realm, apiUrl)
+                .first()
+                .find()
+                ?.token
     }
 
     suspend fun setToken(apiUrl: String, token: String) {
-        monarchy.awaitTransaction { realm ->
-            val scalarTokenEntity = ScalarTokenEntity(apiUrl, token)
-            realm.insertOrUpdate(scalarTokenEntity)
+        realmInstance.write {
+            val scalarTokenEntity = ScalarTokenEntity().apply {
+                this.serverUrl = apiUrl
+                this.token = token
+            }
+            copyToRealm(scalarTokenEntity, updatePolicy = UpdatePolicy.ALL)
         }
     }
 
     suspend fun clearToken(apiUrl: String) {
-        monarchy.awaitTransaction { realm ->
-            ScalarTokenEntity.where(realm, apiUrl).findFirst()?.deleteFromRealm()
+        realmInstance.write {
+            ScalarTokenEntity.where(this, apiUrl).first().find()?.also {
+                delete(it)
+            }
         }
     }
 }
