a762fc1d80aea08a95fc04dae316b86e51437861
Maxime NATUREL <maxime.naturel@niji.fr>
2022-10-03 16:51:38 +0200

    Removing unused imports

--- vector/src/test/java/im/vector/app/core/session/clientinfo/GetMatrixClientInfoUseCaseTest.kt
+++ vector/src/test/java/im/vector/app/core/session/clientinfo/GetMatrixClientInfoUseCaseTest.kt
@@ -17,7 +17,6 @@
 package im.vector.app.core.session.clientinfo
 
 import im.vector.app.test.fakes.FakeSession
-import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
 
