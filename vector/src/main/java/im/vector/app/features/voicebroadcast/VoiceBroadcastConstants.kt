3a951f207604d638bb7ab66453e4d4162e84b32b
Florian Renaud <florianr@element.io>
2022-10-14 16:49:27 +0200

    Add punctuation to kdoc

--- vector/src/main/java/im/vector/app/features/voicebroadcast/VoiceBroadcastConstants.kt
+++ vector/src/main/java/im/vector/app/features/voicebroadcast/VoiceBroadcastConstants.kt
@@ -21,6 +21,6 @@ object VoiceBroadcastConstants {
     /** Voice Broadcast State Event. */
     const val STATE_ROOM_VOICE_BROADCAST_INFO = "io.element.voice_broadcast_info"
 
-    /** Default voice broadcast chunk duration, in seconds */
+    /** Default voice broadcast chunk duration, in seconds. */
     const val DEFAULT_CHUNK_LENGTH = 30
 }
