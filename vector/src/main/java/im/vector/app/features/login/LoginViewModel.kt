75d589bedd0dbff1467b807c343889d69154322a
ByeongsuPark <byeongsu@soongsil.ac.kr>
2022-10-22 00:15:44 +0900

    Add trim to username input on sign-in
    Add trim to username input on the app side and SDK side
    Signed-off-by: ByeongsuPark <byeongsu@soongsil.ac.kr>

--- vector/src/main/java/im/vector/app/features/login/LoginViewModel.kt
+++ vector/src/main/java/im/vector/app/features/login/LoginViewModel.kt
@@ -684,7 +684,7 @@ class LoginViewModel @AssistedInject constructor(
             currentJob = viewModelScope.launch {
                 try {
                     safeLoginWizard.login(
-                            action.username,
+                            action.username.trim(),
                             action.password,
                             action.initialDeviceName
                     )
