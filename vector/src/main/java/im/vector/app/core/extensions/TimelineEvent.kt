ee22dafbc90732003f5944c95a577bcf75ddb7fb
Florian Renaud <florianr@element.io>
2022-11-28 12:14:31 +0100

    Fix regression when getting last message content for Voice Broadcast state event

--- vector/src/main/java/im/vector/app/core/extensions/TimelineEvent.kt
+++ vector/src/main/java/im/vector/app/core/extensions/TimelineEvent.kt
@@ -19,7 +19,6 @@ package im.vector.app.core.extensions
 import im.vector.app.features.voicebroadcast.VoiceBroadcastConstants
 import im.vector.app.features.voicebroadcast.model.MessageVoiceBroadcastInfoContent
 import org.matrix.android.sdk.api.session.events.model.EventType
-import org.matrix.android.sdk.api.session.events.model.toContent
 import org.matrix.android.sdk.api.session.events.model.toModel
 import org.matrix.android.sdk.api.session.room.model.message.MessageContent
 import org.matrix.android.sdk.api.session.room.send.SendState
@@ -41,7 +40,7 @@ fun TimelineEvent.getVectorLastMessageContent(): MessageContent? {
     // Iterate on event types which are not part of the matrix sdk, otherwise fallback to the sdk method
     return when (root.getClearType()) {
         VoiceBroadcastConstants.STATE_ROOM_VOICE_BROADCAST_INFO -> {
-            (annotations?.editSummary?.latestEdit?.getClearContent()?.toModel<MessageContent>().toContent().toModel<MessageVoiceBroadcastInfoContent>()
+            (annotations?.editSummary?.latestEdit?.getClearContent()?.toModel<MessageVoiceBroadcastInfoContent>()
                     ?: root.getClearContent().toModel<MessageVoiceBroadcastInfoContent>())
         }
         else -> getLastMessageContent()
