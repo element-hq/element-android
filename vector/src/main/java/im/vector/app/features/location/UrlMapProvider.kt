96780ec5b73bbfcec497c992ed330ef6c212fee2
Artjom König <artjom.koenig.ext@bwi.de>
2022-12-29 09:34:55 +0100

    fixed copyright info

--- vector/src/main/java/im/vector/app/features/location/UrlMapProvider.kt
+++ vector/src/main/java/im/vector/app/features/location/UrlMapProvider.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022 New Vector Ltd
+ * Copyright (c) 2022 BWI GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
