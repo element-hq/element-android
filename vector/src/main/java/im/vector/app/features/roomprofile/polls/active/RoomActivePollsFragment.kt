f20513eb16602a7b05d02231ca784215f0a28551
Maxime NATUREL <46314705+mnaturel@users.noreply.github.com>
2022-12-30 10:56:44 +0100

    Render the active polls list on fragment

--- vector/src/main/java/im/vector/app/features/roomprofile/polls/active/RoomActivePollsFragment.kt
+++ vector/src/main/java/im/vector/app/features/roomprofile/polls/active/RoomActivePollsFragment.kt
@@ -16,20 +16,63 @@
 
 package im.vector.app.features.roomprofile.polls.active
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.airbnb.mvrx.parentFragmentViewModel
+import com.airbnb.mvrx.withState
 import dagger.hilt.android.AndroidEntryPoint
+import im.vector.app.core.extensions.cleanup
 import im.vector.app.core.platform.VectorBaseFragment
 import im.vector.app.databinding.FragmentRoomPollsListBinding
+import im.vector.app.features.roomprofile.polls.PollSummary
 import im.vector.app.features.roomprofile.polls.RoomPollsViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class RoomActivePollsFragment : VectorBaseFragment<FragmentRoomPollsListBinding>() {
+class RoomActivePollsFragment :
+        VectorBaseFragment<FragmentRoomPollsListBinding>(),
+        RoomActivePollsController.Listener {
+
+    @Inject
+    lateinit var roomActivePollsController: RoomActivePollsController
 
     private val viewModel: RoomPollsViewModel by parentFragmentViewModel(RoomPollsViewModel::class)
 
     override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentRoomPollsListBinding {
         return FragmentRoomPollsListBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+    }
+
+    private fun setupList() {
+        roomActivePollsController.listener = this
+        views.activePollsList.adapter = roomActivePollsController.adapter
+    }
+
+    override fun onDestroyView() {
+        cleanUpList()
+        super.onDestroyView()
+    }
+
+    private fun cleanUpList() {
+        views.activePollsList.cleanup()
+        roomActivePollsController.listener = null
+    }
+
+    override fun invalidate() = withState(viewModel) { viewState ->
+        renderList(viewState.polls.filterIsInstance(PollSummary.ActivePoll::class.java))
+    }
+
+    private fun renderList(polls: List<PollSummary.ActivePoll>) {
+        roomActivePollsController.setData(polls)
+    }
+
+    override fun onPollClicked(pollId: String) {
+        // TODO navigate to details
     }
 }
