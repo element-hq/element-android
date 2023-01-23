a8597f4516944352126597531d086e65db27de26
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-01-24 02:45:51 +0530

    fixed the space prefernce issue in setting:preference

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsPreferencesFragment.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsPreferencesFragment.kt
@@ -103,8 +103,8 @@ class VectorSettingsPreferencesFragment :
         }
 
         findPreference<Preference>(VectorPreferences.SETTINGS_PREF_SPACE_CATEGORY)!!.let { pref ->
-            pref.isVisible = !vectorFeatures.isNewAppLayoutFeatureEnabled()
-            pref.isEnabled = !vectorPreferences.isNewAppLayoutEnabled()
+            pref.isVisible = vectorFeatures.isNewAppLayoutFeatureEnabled()
+            pref.isEnabled = vectorFeatures.isNewAppLayoutFeatureEnabled()
         }
 
         // Url preview
