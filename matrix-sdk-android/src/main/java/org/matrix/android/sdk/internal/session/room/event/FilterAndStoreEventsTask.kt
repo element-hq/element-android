030e37655e82c12a51488395674399ccef1a2af9
Maxime NATUREL <46314705+mnaturel@users.noreply.github.com>
2023-01-26 13:50:46 +0100

    Fixing unit tests in SDK

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/room/event/FilterAndStoreEventsTask.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/room/event/FilterAndStoreEventsTask.kt
@@ -58,7 +58,6 @@ internal class DefaultFilterAndStoreEventsTask @Inject constructor(
 
     private suspend fun addMissingEventsInDB(roomId: String, events: List<Event>) {
         monarchy.awaitTransaction { realm ->
-            // TODO we should insert TimelineEventEntity as well, how to do that????
             val eventIdsToCheck = events.mapNotNull { it.eventId }.filter { it.isNotEmpty() }
             if (eventIdsToCheck.isNotEmpty()) {
                 val existingIds = EventEntity.where(realm, eventIdsToCheck).findAll().toList().map { it.eventId }
