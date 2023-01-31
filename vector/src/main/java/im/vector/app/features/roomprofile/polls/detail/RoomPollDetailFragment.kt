cbd97d440a808d9a7111ba1aeaebb28d56ad4956
Onuray Sahin <onuray.sahin@gmail.com>
2023-01-31 12:19:30 +0300

    Render poll detail.

--- vector/src/main/java/im/vector/app/features/roomprofile/polls/detail/RoomPollDetailFragment.kt
+++ vector/src/main/java/im/vector/app/features/roomprofile/polls/detail/RoomPollDetailFragment.kt
@@ -26,14 +26,18 @@ import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
 import dagger.hilt.android.AndroidEntryPoint
 import im.vector.app.R
+import im.vector.app.core.extensions.configureWith
 import im.vector.app.core.platform.VectorBaseFragment
 import im.vector.app.databinding.FragmentRoomPollDetailBinding
 import im.vector.app.features.roomprofile.RoomProfileArgs
 import im.vector.app.features.roomprofile.polls.RoomPollsType
 import im.vector.app.features.roomprofile.polls.RoomPollsViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RoomPollDetailFragment : VectorBaseFragment<FragmentRoomPollDetailBinding>() {
+
+    @Inject lateinit var roomPollDetailController: RoomPollDetailController
 
     private val viewModel: RoomPollsViewModel by activityViewModel()
     private val roomProfileArgs: RoomProfileArgs by args()
@@ -45,11 +49,14 @@ class RoomPollDetailFragment : VectorBaseFragment<FragmentRoomPollDetailBinding>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initToolbar()
+        views.pollDetailRecyclerView.configureWith(
+                roomPollDetailController,
+                hasFixedSize = true,
+        )
     }
 
-    private fun initToolbar() = withState(viewModel) { state ->
-        val title = if (state.selectedRoomPollsType == RoomPollsType.ACTIVE) getString(R.string.room_polls_active)
+    private fun setupToolbar(roomPollsType: RoomPollsType) {
+        val title = if (roomPollsType == RoomPollsType.ACTIVE) getString(R.string.room_polls_active)
                 else getString(R.string.room_polls_ended)
 
         setupToolbar(views.roomPollDetailToolbar)
@@ -58,7 +65,10 @@ class RoomPollDetailFragment : VectorBaseFragment<FragmentRoomPollDetailBinding>
     }
 
     override fun invalidate() = withState(viewModel) { state ->
+        setupToolbar(state.selectedRoomPollsType)
+
         state.getSelectedPoll()?.let { _ ->
+            roomPollDetailController.setData(state)
         }
         Unit
     }
