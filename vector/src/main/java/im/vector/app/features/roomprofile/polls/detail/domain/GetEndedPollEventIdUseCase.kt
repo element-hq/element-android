9584eb496a86e2ee561898e2b2d458eb5b826bfd
Maxime NATUREL <46314705+mnaturel@users.noreply.github.com>
2023-02-03 12:02:56 +0100

    Adding unit tests for GetEndedPollEventIdUseCase

--- vector/src/main/java/im/vector/app/features/roomprofile/polls/detail/domain/GetEndedPollEventIdUseCase.kt
+++ vector/src/main/java/im/vector/app/features/roomprofile/polls/detail/domain/GetEndedPollEventIdUseCase.kt
@@ -19,20 +19,23 @@ package im.vector.app.features.roomprofile.polls.detail.domain
 import im.vector.app.core.di.ActiveSessionHolder
 import org.matrix.android.sdk.api.session.events.model.RelationType
 import org.matrix.android.sdk.api.session.events.model.isPollEnd
+import timber.log.Timber
 import javax.inject.Inject
 
-// TODO add unit tests
 class GetEndedPollEventIdUseCase @Inject constructor(
         private val activeSessionHolder: ActiveSessionHolder,
 ) {
 
     fun execute(roomId: String, startPollEventId: String): String? {
-        return activeSessionHolder.getActiveSession()
-                .roomService()
-                .getRoom(roomId)
-                ?.timelineService()
-                ?.getTimelineEventsRelatedTo(RelationType.REFERENCE, startPollEventId)
-                ?.find { it.root.isPollEnd() }
-                ?.eventId
+        val result = runCatching {
+            activeSessionHolder.getActiveSession()
+                    .roomService()
+                    .getRoom(roomId)
+                    ?.timelineService()
+                    ?.getTimelineEventsRelatedTo(RelationType.REFERENCE, startPollEventId)
+                    ?.find { it.root.isPollEnd() }
+                    ?.eventId
+        }.onFailure { Timber.w("failed to retrieve the ended poll event id for eventId:$startPollEventId") }
+        return result.getOrNull()
     }
 }
