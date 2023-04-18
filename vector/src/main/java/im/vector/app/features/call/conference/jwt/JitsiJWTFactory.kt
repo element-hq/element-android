28da02c583c2eae062b2b75de178b2c530b610f3
Alexey Nechaev <11815484+tomtit@users.noreply.github.com>
2023-04-18 19:45:39 +0300

    Fixes #7758: Fixed JWT token for Jitsi openidtoken-jwt authentication
    Signed-off-by: Alexey Nechaev <seysane@yahoo.com>

--- vector/src/main/java/im/vector/app/features/call/conference/jwt/JitsiJWTFactory.kt
+++ vector/src/main/java/im/vector/app/features/call/conference/jwt/JitsiJWTFactory.kt
@@ -19,8 +19,11 @@ package im.vector.app.features.call.conference.jwt
 import im.vector.app.core.utils.ensureProtocol
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.io.Encoders
 import io.jsonwebtoken.security.Keys
 import org.matrix.android.sdk.api.session.openid.OpenIdToken
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 import javax.inject.Inject
 
 class JitsiJWTFactory @Inject constructor() {
@@ -37,7 +40,12 @@ class JitsiJWTFactory @Inject constructor() {
             userDisplayName: String
     ): String {
         // The secret key here is irrelevant, we're only using the JWT to transport data to Prosody in the Jitsi stack.
-        val key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+        // In the PR https://github.com/jitsi/luajwtjitsi/pull/3 the function `luajwtjitsi.decode` was removed and
+        // we cannot use random secret keys anymore. But the JWT library `jjwt` doesn't accept the hardcoded key `notused`
+        // from the module `prosody-mod-auth-matrix-user-verification` since it's too short and thus insecure. So, we
+        // create a new token using a random key and then re-sign the token manually with the 'weak' key.
+        val signatureAlgorithm = SignatureAlgorithm.HS256
+        val key = Keys.secretKeyFor(signatureAlgorithm)
         val context = mapOf(
                 "matrix" to mapOf(
                         "token" to openIdToken.accessToken,
@@ -52,7 +60,8 @@ class JitsiJWTFactory @Inject constructor() {
         // As per Jitsi token auth, `iss` needs to be set to something agreed between
         // JWT generating side and Prosody config. Since we have no configuration for
         // the widgets, we can't set one anywhere. Using the Jitsi domain here probably makes sense.
-        return Jwts.builder()
+        val token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
                 .setIssuer(jitsiServerDomain)
                 .setSubject(jitsiServerDomain)
                 .setAudience(jitsiServerDomain.ensureProtocol())
@@ -61,5 +70,11 @@ class JitsiJWTFactory @Inject constructor() {
                 .claim("context", context)
                 .signWith(key)
                 .compact()
+        // Re-sign token with the hardcoded key
+        val toSign = token.substring(0, token.lastIndexOf('.'))
+        val mac = Mac.getInstance(signatureAlgorithm.jcaName)
+        mac.init(SecretKeySpec("notused".toByteArray(), mac.algorithm))
+        val prosodySignature = Encoders.BASE64URL.encode(mac.doFinal(toSign.toByteArray()))
+        return "$toSign.$prosodySignature"
     }
 }
