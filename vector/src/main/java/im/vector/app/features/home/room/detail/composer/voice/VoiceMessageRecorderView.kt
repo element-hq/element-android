2f298d86511f9110ff7f0609c25ebaab6a210b9c
Odin Vex <44311901+OdinVex@users.noreply.github.com>
2024-03-12 02:43:20 +0000

    Adjust voice-recording seconds remaining notification to 30 seconds
    10 seconds is barely noticed until you're part-way through it, 30 seconds could help users with accessibility issues.

--- vector/src/main/java/im/vector/app/features/home/room/detail/composer/voice/VoiceMessageRecorderView.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/composer/voice/VoiceMessageRecorderView.kt
@@ -205,7 +205,7 @@ class VoiceMessageRecorderView @JvmOverloads constructor(
             post {
                 callback.onRecordingLimitReached()
             }
-        } else if (timeDiffToRecordingLimit in 10_000..10_999) {
+        } else if (timeDiffToRecordingLimit in 30_000..30_999) {
             post {
                 val secondsRemaining = floor(timeDiffToRecordingLimit / 1000f).toInt()
                 voiceMessageViews.renderToast(context.getString(R.string.voice_message_n_seconds_warning_toast, secondsRemaining))
